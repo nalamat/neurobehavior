314a997788be7e97982122c0d051cd04b16e44af
bburan <bburan@localhost>
2010-07-30 17:36:03 -0400

    Checkin of positive behavior code for testing

--- cns/experiment/controller/positive_controller.py
+++ cns/experiment/controller/positive_controller.py
@@ -28,21 +28,10 @@ class CurrentSettings(HasTraits):
     '''Tracks the trial index for comparision with the circuit index'''
     idx = CInt(0)
 
-    '''Number of safe trials''' 
-    safe_trials = CInt
-    '''The trial we are going to, or currently, presenting'''
-    trial = CInt
-
     '''Parameter to present'''
     par = CFloat
-    par_remind = CFloat
-
-    _choice_num_safe = Any
     _choice_par = Any
     _signal_cache = {}
-
-    signal_warn = Property
-    signal_remind = Property
 
     def __init__(self, **kw):
         super(CurrentSettings, self).__init__(**kw)
@@ -53,26 +42,19 @@ class CurrentSettings(HasTraits):
 
     def initialize(self, paradigm):
         self._choice_par = self._get_choice_par(paradigm)
-        self._choice_num_safe = self._get_choice_num_safe(paradigm)
         self._signal_cache = build_signal_cache(self.signal_warn,
                                                 paradigm.par_remind,
                                                 *paradigm.pars)
-
-        self.build_shock_level_cache(paradigm)
-        self.par_remind = paradigm.par_remind
         self.next()
 
     def next(self):
         self.par = self._choice_par.next()
-        self.safe_trials = self._choice_num_safe.next()
-        self.trial = 1
-        self.shock_level = self.paradigm.shock_settings.get_level(self.par)
 
     #===========================================================================
     # Helpers for run-time control of experiment
     #===========================================================================
     def _get_signal(self):
-        return self._signal_warn_cache[self.par]
+        return self._signal_cache[self.par]
 
     def _get_choice_par(self, paradigm):
         # Always pass a copy of pars to other functions that may modify the
@@ -87,14 +69,7 @@ class PositiveController(ExperimentController):
     circuit = Any
     pump = Any
 
-    exp_node = Any
-    data_node = Any
-
-    # Will be used to poll the state of the hardware every few milliseconds and
-    # update the view as needed (i.e. download lick data, upload new signal
-    # waveforms, etc).  See timer_tick.
     fast_timer = Instance(Timer)
-    slow_timer = Instance(Timer)
 
     # A coroutine pipeline that acquires contact data from the RX6 and sends it
     # to the TrialData object
@@ -103,21 +78,10 @@ class PositiveController(ExperimentController):
     completed = Bool(False)
     water_infused = Float(0)
 
-    status = Property(Str, depends_on='current.trial, current.par, current.safe_trials, state')
+    status = Property(Str, depends_on='currentnt.par, state')
     time_elapsed = Property(Str, depends_on='slow_tick', label='Time')
 
-    slow_tick = Event
     fast_tick = Event
-
-    def init(self, info):
-        # Install the toolbar handler
-        self.model = info.object
-        self.toolbar.install(self, info)
-        try:
-            self.init_equipment(info)
-        except equipment.EquipmentError, e:
-            self.state = 'disconnected'
-            error(info.ui.control, str(e))
 
     def init_equipment(self, info):
         self.pump = equipment.pump().Pump()
@@ -125,17 +89,7 @@ class PositiveController(ExperimentController):
         self.circuit = self.backend.load('positive-behavior', 'RX6')
 
     def configure_circuit(self, circuit, paradigm):
-        if circuit is None:
-            return
-        elif circuit.running:
-            raise SystemError, 'Cannot configure circuit while it is running'
-
-        # This is an example of the "handler" approach.  The model
-        # ('paradigm') does not have to concern itself about how the
-        # equipment needs to be configured.  If we ever get a new set of
-        # hardware, this code would be the only stuff that needs changing.
         circuit.reload()
-
         circuit.pretrial_delay_n.set(paradigm.pretrial_delay, 'msec')
         circuit.reward_delay_n.set(paradigm.reward_delay, 'msec')
         circuit.reward_duration_n.set(paradigm.reward_duration, 'msec')
@@ -148,22 +102,9 @@ class PositiveController(ExperimentController):
             return
 
         try:
-            # Order is important.  The data depends on several properties set in
-            # the circuit, so initialize_data must be called after
-            # initialize_circuit
             self.configure_circuit(self.circuit, self.model.paradigm)
-
-            #===================================================================
-            # Finally, everything's a go!
-            #===================================================================
             self.fast_timer = Timer(250, self.tick, 'fast')
-            self.slow_timer = Timer(1000, self.tick, 'slow')
-
-            # Setting state to paused should be one of the last things we do to
-            # prevent the UI from changing the controls to the 'running' state.
-            self.state = 'paused'
             self.circuit.start()
-
         except BaseException, e:
             self.state = 'halted'
             error(self.info.ui.control, str(e))
@@ -171,17 +112,13 @@ class PositiveController(ExperimentController):
 
     def remind(self, info=None):
         self.state = 'manual'
-        # The actual sequence is important.  We must finish uploading the signal
-        # before we set the circuit flags to allow commencement of a trial.
-
-        self.circuit.target_signal.set(self.current.target_signal)
-        self.backend.set_attenuation(self.current.signal_remind.attenuation, 'PA5')
-        self.circuit.pause_state.value = False # Everything's ready. GO!
+        signal = self.current.target_signal
+        self.circuit.target_signal.set(signal)
+        self.backend.set_attenuation(signal.attenuation, 'PA5')
         self.circuit.trigger(1)
 
     def stop(self, info=None):
         self.state = 'halted'
-        self.slow_timer.stop()
         self.fast_timer.stop()
         self.circuit.stop()
         self.pending_changes = {}
@@ -192,31 +129,14 @@ class PositiveController(ExperimentController):
     #===========================================================================
     @on_trait_change('slow_tick')
     def task_update_pump(self):
-        infused = self.pump.infused
-        self.model.data.log_water(self.circuit.ts_n.value, infused)
-        self.water_infused = infused
+        self.water_infused = self.pump.infused
 
     def _get_status(self):
         if self.state == 'disconnected':
             return 'Cannot connect to equipment'
         if self.state == 'halted':
             return 'System is halted'
-        if self.state == 'manual':
-            return 'PAUSED: presenting reminder (%r)' % self.current.par
-
-        if self.current.trial > self.current.safe_trials:
-            status = 'WARNING (%r)' % self.current.par
-        else:
-            mesg = 'SAFE %d of %d (%r)'
-            status = mesg % (self.current.trial, self.current.safe_trials, self.current.par)
-        if self.state == 'paused':
-            status = 'PAUSED: next trial is %s' % status
-        return status
-
-    @on_trait_change('pump.rate')
-    def log_pump(self, new):
-        if self.state <> 'halted':
-            self.model.data.log_event(self.circuit.ts_n.value, 'pump rate', new)
+        return 'Target (%r)' % self.current.par
 
     ############################################################################
     # Code to apply parameter changes
