1b1363df7d269509d663cac3d6a38454c56759a4
bburan <bburan@localhost>
2010-08-16 10:26:05 -0400

    checkin of missing files for positive behavior as well as several experimental TDT macros

---
+++ cns/experiment/api.py
@@ -0,0 +1,6 @@
+from .controller.experiment_controller import ExperimentController
+from .controller.aversive_controller import AversiveController
+from .controller.positive_controller import PositiveController
+from .paradigm.aversive_paradigm import AversiveParadigm
+from .paradigm.positive_paradigm import PositiveParadigm
+from .data.aversive_data import AversiveData
