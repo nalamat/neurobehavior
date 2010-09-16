a92cbd46da41ce88cbd19d4d89bfea0f40ed5d53
bburan <bburan@localhost>
2010-09-16 18:23:17 -0400

    Moved code for launching experiment from cohort view to a cns.experiment and made it more generic (so it can handle various experiment types)

--- app/run_aversive_FM_behavior.py
+++ app/run_aversive_FM_behavior.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
     from config import settings
     from cns.experiment import load_experiment_launcher
-    load_experiment_launcher('AversiveExperiment')
+    load_experiment_launcher('AversiveFMExperiment')
