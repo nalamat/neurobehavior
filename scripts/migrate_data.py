d758c0978ebdc3e164ad4a208e3f8ccafdc0c9b4
bburan <bburan@localhost>
2010-10-03 15:21:08 -0400

    Script to convert aversive data from v1 to v2 format

--- scripts/migrate_data.py
+++ scripts/migrate_data.py
@@ -12,6 +12,9 @@ the Data node (as well as stop_time).  This function scans the file for
 experiment data missing the start_time and stop_time attributes.  If missing,
 the start_time is derived from the name of the node, while stop_time is
 estimated by checking the last timestamp recorded in that experiment.
+
+Repair functions must be run in a very specific order that's listed in the tuple
+below.
 '''
 
 import tables
@@ -20,7 +23,9 @@ log = logging.getLogger(__name__)
 
 def fix_node_class(node):
     '''
-    Fix node class
+    Updates node class to RawAversiveData_v0_2.  Assumes that all "cleanup"
+    functions have successfully been completed and the node, indeed, reflects
+    v0_2 structure.
     '''
     try:
         if node._v_attrs.klass == 'AversiveData':
@@ -32,55 +37,32 @@ def fix_node_class(node):
     except:
         pass
 
-def fix_node_metadata(node):
+def fix_node_time(node):
     '''
-    Fix missing start_time, stop_time and duration attribute
+    Fix missing start_time, stop_time and duration attribute.  Depends on
+    new-style contact data.
     '''
     from cns.data.h5_utils import extract_date_from_name
     from cns.data import persistence
     from datetime import timedelta
 
-    name = node._v_name
-    if name.startswith('aversive_date_'):
+    if node._v_name.startswith('aversive_date_'):
         try: 
             node.Data._v_attrs.start_time
         except:
-            if name.startswith('date'):
-                start_time = extract_date_from_name(node, pre='date')
-            else:
-                start_time = extract_date_from_name(node, 
-                                                    pre='aversive_date_')
-            print node._v_pathname
-            print start_time
+            start_time = extract_date_from_name(node, pre='aversive_date_')
             try:
-                fs = node.Data.contact._v_attrs.fs
-            except:
-                # Trial running is the only array that is reliably present
-                # in kall of the channel types
                 fs = node.Data.contact.trial_running._v_attrs.fs
-                
-            try:
-                ts = node.Data.water_log[-1][0]
+                ts = node.Data.trial_log[-1][0]
             except:
-                try:
-                    ts = node.Data.trial_data[-1][0]
-                except:
-                    print 'aborted experiment, setting ts to 0'
-                    ts = 0
+                print 'aborted experiment, setting ts to 0'
+                ts = 0
             
-            try:
-                # Guess duration of experiment from last timestamp recorded
-                # in the water log and computing number of seconds based on
-                # sampling frequency.
-                duration = timedelta(seconds=ts/fs)
-                stop_time = start_time + duration
-                node.Data._v_attrs.start_time = persistence.strftime(start_time)
-                node.Data._v_attrs.stop_time = persistence.strftime(stop_time)
-                #node.Data._v_attrs.duration = duration
-                print 'Recovered duration for ', node._v_pathname
-            except BaseException, e:
-                print 'Unable to recover duration for ', node._v_pathname
-                print e
+            duration = timedelta(seconds=ts/fs)
+            stop_time = start_time + duration
+            node.Data._v_attrs.start_time = persistence.strftime(start_time)
+            node.Data._v_attrs.stop_time = persistence.strftime(stop_time)
+            print 'Recovered duration for ', node._v_pathname
 
 def fix_node_contact(node):
     '''
@@ -109,50 +91,80 @@ def fix_node_contact(node):
             channel = FileChannel(node=contact_node, name=name, fs=fs,
                                   dtype=dtype)
             channel.write(node[:,i])
-
         node._f_remove()
 
+def move_experiment_nodes(node):
+    '''
+    Some nodes were originally stored directly under the animal node or in a
+    subgroup called behavior_experiments.  These nodes are moved to the
+    experiments folder and the behavior_experiments subgroup is deleted.
+    '''
+    from cns.data.h5_utils import get_or_append_node
+
+    if node._v_name == 'behavior_experiments':
+        newparent = get_or_append_node(node._v_parent, 'experiments')
+        for child in node:
+            child._f_move(newparent=newparent)
+            print 'Moved to experiments: ' + child._v_name
+        node._f_remove()
+
+    elif node._v_name.startswith('Animal'):
+        newparent = get_or_append_node(node, 'experiments')
+        for child in node:
+            if 'date' in child._v_name:
+                print 'Moved to experiments: ' + child._v_name
+                child._f_move(newparent=newparent)
+
 def fix_node_names(node):
-    '''
-    Update old naming conventions to match new naming conventions.
-    '''
-    import re
+    import re 
 
     if re.match('^date(\d+)', node._v_name):
-        print 'Renaming node %s' % node._v_pathname
-        node._f_move(newname='aversive_date_' + node._v_name[4:],
-                     newparent=node._v_parent.experiments)
-        node.AversiveData._f_move(newname='Data')
-        node.AversiveParadigm_0._f_move(newname='Paradigm')
-        node.Data.AnalyzedAversiveData_0._f_move(newparent=node._v_pathname + \
-                                                 '/Data/Analyzed',
-                                                 createparents=True)
+        print 'Renaming node: ' + node._v_pathname
+        node._f_move(newname='aversive_date_' + node._v_name[4:])
 
-    try:
+    if node._v_name == 'AversiveData':
+        print 'Renaming node: ' + node._v_pathname
+        node._f_move(newname='Data')
+
+    if node._v_name == 'AversiveParadigm_0':
+        print 'Renaming node: ' + node._v_pathname
+        node._f_move(newname='Paradigm')
+
+def move_analyzed_nodes(node):
+
+    if node._v_pathname.endswith('Data/AnalyzedAversiveData_0'):
+        newparent = node._v_parent._v_pathname + '/Analyzed'
+        node._f_move(newparent=newparent, createparents=True)
+
+def fix_node_attr_names(node):
+
+    if hasattr(node, '_v_children'):
         if 'trial_data' in node._v_children:
-            print 'Renaming trial_log and trial_data in node %s' % node._v_pathname
+            print 'Renaming trial_data: ' + node._v_name
             if 'trial_log' in node._v_children:
+                print 'Renaming trial_log: ' + node._v_name
                 node.trial_log._f_move(newname='event_log')
             node.trial_data._f_move(newname='trial_log')
-    except AttributeError:
-        pass
+
+cleanup = (move_experiment_nodes,
+           move_analyzed_nodes,
+           fix_node_attr_names,
+           fix_node_names,
+           fix_node_contact,
+           fix_node_time,
+           fix_node_class,
+          )
 
 def fix_legacy_data(file):
-    f = tables.openFile(file, 'a')
-    log.info('Updating node class metadata')
-    for node in f.walkNodes():
-        fix_node_class(node)
-    log.info('Updating node names and paths')
-    for node in f.walkNodes():
-        fix_node_names(node)
-    log.info('Updating node contact data')
-    for node in f.walkNodes():
-        fix_node_contact(node)
-    log.info('Updating missing node metadata')
-    for node in f.walkNodes():
-        fix_node_metadata(node)
+    d = tables.openFile(file, 'a')
+    for f in cleanup:
+        print 'Running function ' + f.__name__
+        for node in d.walkNodes():
+            f(node)
 
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) == 2:
-        fix_legacy_data(sys.argv[1])
+    import argparse
+    parser = argparse.ArgumentParser(description='Migrate data to v2 format')
+    parser.add_argument('file', type=str, nargs=1)
+    op = parser.parse_args()
+    fix_legacy_data(op.file[0])
