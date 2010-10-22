136da096d0850e6135732ee8e7fd1a70a8a92027
Brad Buran <bburan@alum.mit.edu>
2010-10-22 00:05:30 -0400

    Documented demo script for Merri, Sharad and Daniel

--- demo/play_signals_v2.py
+++ demo/play_signals_v2.py
@@ -10,25 +10,93 @@ log.handlers.append(logging.StreamHandler())
 
 from cns import equipment
 
-# Python files can either be imported as part of another program or executed
-# directly via the command line.  __name__ is a variable available to every
-# script that tells the script whether it is being launched directly or being
-# imported by another program.
-# 
-# This is a very neat trick for testing code.  If you are writing a module of
-# functions you can write some test or demo functions that are run whenever you
-# execute the file directly, but not when you import the functions for use by
-# another script.
-#
-# >>> if __name__ == '__main__':
-# >>>     run_demo_code()
-# >>>     run_test_code()
+'''
+All waveforms uploaded to the DSP buffers must have any necessary onset delays
+and intertrial durations included as part of the waveform (e.g. as an array of
+zeros).  
+
+This circuit implements a dual-buffer scheme for all buffers.  There is always
+one active buffer (a or b) that is being played out.  The other buffer is
+considered the reserve buffer and may be safely written to.  Once the data has
+been uploaded, fire the appropriate trigger to switch over.  Note that, at
+present, you cannot switch buffers individually.  When a switch is requested,
+all buffers will switch from a-to-b or b-to-a simultaneously.
+
+...............<============>....................
+[ onset delay ][   token    ][   offset delay   ]
+[ trial duration                                ]
+^ beginning of waveform (e.g. sample 0)
+               ^ beginning of token (e.g. sample 160)
+                            ^ end of token (e.g. sample 290)
+                                                ^ end of waveform
+
+Typically you will want to generate event times (e.g. TTL triggers) during some
+feature of the waveform.  There are six TTL buffers available (TTL1 ... TTL6)
+that accepts an array of integers.  Each number indicates a sample number
+(relative to the beginning of the waveform) at which a TTL should be generated.
+The corresponding variable, TTL1_dur_n ... TTL6_dur_n indicates the number of
+samples the TTL should be high.  When the buffer becomes active, the first
+number in the array is popped.  As soon as the TTL fires, the next number is
+removed from the stack.
+
+DSP circuit buffers
+-------------------
+DACNa and DACNb : array-like
+    Token waveforms for speakers 1-2.  Substitute N with the speaker number
+    (e.g. DAC1a, DAC2b).
+TTLNa and TTLNb : array-like
+    Event times (relative to start of trial) for TTLs 1-6.  Substitute N with
+    the TTL number (e.g. TTL3a).
+
+DSP circuit variables
+---------------------
+switch : int
+    Bitmask for the TTL that indicates that the circuit may switch buffers
+reset : int
+    Bitmaks for the TTL that indicates the circuit should reset the sample
+    counter and start playing the buffer from the beginning
+repetitions : int
+    Number of repetitions of the same buffer before the circuit should halt
+buffer : int (readonly)
+    Active buffer (0=a, 1=b)
+
+DSP circuit triggers
+--------------------
+soft 1
+    Reset circuit immediately but do not switch buffers
+soft 2
+    Halt circuit
+soft 3
+    Reset circuit immediately and switch buffers
+soft 4
+    Switch buffers once switch condition is met
+soft 5 through 10
+    unused
+
+When searching the parameter space, you would typically write the requested
+waveform to the reserve buffer then fire trigger 3 once the data is uploaded.
+When presenting a predetermined sequence of waveforms, you would typically write
+the next waveform in the sequence to the reserve buffer then fire trigger 4.
+Once trigger 4 is fired, you would then have to listen for a change to
+`buffer` (which indicates that the buffer has switched over and that you
+can now upload the next waveform to the now-reserve buffer).
+'''
 
 from cns.signal.type import Tone, AMNoise, Noise, Silence, BandlimitedNoise
 from cns.signal.util import cos2taper
 import time
 
-def main_sequence():
+def demo_preset_sequence():
+    '''
+    This demo explores how we would generate and upload a preset sequence to the
+    DSP.  This was mainly motivated out of my concern for ensuring that we could
+    present a very rapid sequence of pips (e.g. ABRs are 10 ms pips presented at
+    40/sec) with the appropriate event times.  In essence, the way this works is
+    we chain together the waveforms for the sequence, upload it as one segment,
+    and generate an array of event times for TTL1 that fires at the start of
+    each pip.
+    '''
+
     circuit = equipment.dsp('TDT').load('output-sequence', 'RX6')
     fs = circuit.fs
 
@@ -42,6 +110,7 @@ def main_sequence():
     sequence_triggers = []
     silence = zeros(int((trial_duration-token_duration)*fs))
 
+    # Generate the sequence of pips
     for i in range(5):
         n_ramp = 2
         tone = cos2taper(Tone(duration=token_duration, fs=fs).signal, n_ramp)
@@ -50,24 +119,25 @@ def main_sequence():
         noise = cos2taper(Noise(duration=token_duration, fs=fs).signal, n_ramp)
         signal_sequence.append(noise)
         signal_sequence.append(silence)
+        # Ensure that the trigger fires at the start of each pip
         trial_triggers.append(len(tone)+len(silence))
         trial_triggers.append(len(noise)+len(silence))
 
     signal_sequence.append(zeros(int(fs)))
 
+    # Concatenate the sequence into a single waveform to be uploaded
     full_waveform = concatenate(signal_sequence)
     sequence_triggers.append(len(full_waveform))
 
     circuit.start()
 
-
+    # Upload the data
     circuit.DAC1a.set(full_waveform)
     circuit.TTL1a.set(trial_triggers)
     circuit.TTL3a.set(sequence_triggers)
-    print trial_triggers
-    print sequence_triggers
 
-    circuit.trigger(1) # Apply changes and start
+    # Start running
+    circuit.trigger(1)
 
     # While circuit is playing out first sequence, prepare second sequence
     signal_sequence = []
@@ -89,126 +159,37 @@ def main_sequence():
     circuit.TTL1b.set(trial_triggers)
     circuit.TTL3b.set(sequence_triggers)
 
-    # Changeover when TTL3 fires
+    # Tell circuit to switch to buffer b and play the new waveform when it's
+    # done with the current buffer (i.e. when TTL3 fires).  Note that TTL3 has a
+    # bitmask of 000100.  The integer representation of this bitmask is 4.
     circuit.switch.set(4) 
     circuit.trigger(4)
 
-    #while 1:
-        #print "A", circuit.TTL1a_value.value
-        #print "B", circuit.TTL3a_value.value
-
+    # When a DSP circuit is first loaded, it registers a program exit hook.
+    # When the program exits, it calls this hook to properly shut down the DSP.
     raw_input("Enter to quit")
 
-def main():
-    #---------------------------------------------------------------------------
+def demo_mouse_mode():
+    '''
+    This demonstrates the speed at which we can compute a waveform and upload it
+    to the DSP when the user requests a change.  In the absence of a GUI, we use
+    the command prompt to request the changes.
+    '''
+
     # Load the circuit
-    #---------------------------------------------------------------------------
-
-    #---------------------------------------------------------------------------
-    # Configure circuit defaults
-    #---------------------------------------------------------------------------
-    '''
-    The DSP circuit has
-
-    DSP circuit paramters
-    ---------------------
-    Trial structure
-        token_del   ---------
-        token_dur            <=========>
-        post_del                        ----------------
-        trial_dur   ------------------------------------
-
-    reps : int
-        Number of times to repeat buffer (-1=continuous)
-    token_dur_n : int
-        Duration of sound token in samples.  Typically this should be set to the
-        length of the waveform.
-    token_del_n : int
-        Number of samples to delay sound token.  This is typicaly used when you
-        want to generate some event (e.g. a NMR pulse) before the token is
-        played.
-    rep_dur_n : int
-        Duration of the entire trial.  Includes token_del_n and token_dur_n.
-        Note that post_del (the silent period following presentation of the
-        sound token) is implicit in rep_dur_n-(token_del_n+token_dur_n).
-    buffer : int (read only)
-        Indicates active buffer, 0 = a, 1 = b
-
-    It is your responsibility to ensure that (token_del + token_dur) >=
-    trial_dur.
-
-    All of these variables are guarded by latches.  Their values will not be
-    applied until you fire a software trigger (2 or 3).  See the section on DSP
-    circuit triggers for more information.
-
-    DSP circuit buffers
-    -------------------
-    This circuit implements a dual-buffer scheme for all buffers.  There is
-    always one active buffer (a or b) that is being played out.  The other
-    buffer is considered the reserve buffer and may be safely written to.  Once
-    the data has been uploaded, fire the appropriate trigger to switch over.
-    Note that, at present, you cannot switch buffers individually.  When a
-    switch is requested, all buffers will switch from a to b or b to a
-    simultaneously.
-    
-    DACNa and DACNb : array-like
-        Token waveforms for speakers 1-2.  Substitute N with the speaker number
-        (e.g. DAC1a, DAC2b).
-    TTLNa and TTLNb : array-like
-        Event times (relative to start of trial) for TTLs 3-6.  Substitute N
-        with the TTL number (e.g. TTL3a).  Note that the first two TTLs are
-        reserved.
-
-    DSP circuit triggers
-    --------------------
-    soft 1
-        Unpause (note that when loaded, circuit is in a paused state so this
-        trigger must be fired once you've configured the circuit)
-    soft 2
-        Switch buffers and apply new parameters now
-    soft 3
-        Apply new parameters now, but don't switch buffers
-    soft 4
-        Switch buffers and apply new parameters when current trial is over
-    soft 5
-        Apply new parameters when current trial is over, but don't switch
-        buffers
-    soft 6
-        Pause
-    soft 7 through 10
-        unused
-
-    Triggers 2 and 3 end the current trial and apply the requested changes
-    immediately.  Triggers 4 and 5 will apply the requested changes once the
-    current trial is over.  When searching the parameter space, you would
-    typically write the new waveform to the reserve buffer then fire trigger 2
-    once the data is uploaded.  When presenting a predetermined sequence of
-    waveforms, you would typically write the next waveform in the sequence to
-    the reserve buffer then fire trigger 4.  Once trigger 4 is fired, you would
-    then have to listen for a change to circuit.buffer (which indicates that the
-    buffer has switched over and that you can now upload the next waveform to
-    the now-reserve buffer).
-    
-    TODO: This dual-buffer paradigm seems to work fine for long-duration tokens.
-    However, I need to test this with shorter-duration tokens (e.g. total trial
-    duration 25 ms).
-
-    '''
-
     circuit = equipment.dsp('TDT').load('output-sequence', 'RX6')
     circuit.start() # Note that circuit is still in paused state
     fs = circuit.fs
 
-    #---------------------------------------------------------------------------
-    # Play!!!!
-    #---------------------------------------------------------------------------
     token_delay = 0
     token_duration = 1
     trial_duration = 2
 
     circuit.trigger(1)
 
+    # Reset when TTL1 fires
     circuit.reset.set(1)
+    # Switch when TTL1 fires
     circuit.switch.set(1)
 
     import textwrap
@@ -225,30 +206,32 @@ def main():
     mode = 3
 
     def set_signal(signal):
-        # Apply tapered envelope to minimize spectral splatter
         from numpy import zeros, r_, array
 
+        # Apply tapered envelope to minimize spectral splatter
         signal = cos2taper(signal.signal, int(0.1*fs))
-
         pad_pre = zeros(int(token_delay*fs))
         pad_post = zeros(int((trial_duration-token_delay-token_duration)*fs))
 
-        signal = r_[pad_pre, signal, pad_post]
-
-        # This circuit implements a dual-buffer scheme.  It will continue to
-        # play data from one buffer until it recieves a trigger to switch to the
-        # new buffer.  The buffers are named DAC1a and DAC1b.  First, check to
-        # see which buffer is current and write data to the reserve buffer.
-        # Once the data is written, send a software trigger (2) to tell the
-        # circuit to switch to the new buffer.
+        # First, check to see which buffer is current and write data to the
+        # reserve buffer.  Once the data is written, send a software trigger 
+        # to tell the circuit to switch to the new buffer.  When mode == 3,
+        # circuit switches immediately, when mode == 4, circuit switches when
+        # switch condition is met.
         if circuit.buffer.value == 1:
             circuit.DAC1a.set(signal)
-            circuit.TTL1a.set([len(signal)])
+            circuit.TTL1a.set([len(signal)]) # Fire TTL1 at end of waveform
         else:
             circuit.DAC1b.set(signal)
-            circuit.TTL1b.set([len(signal)])
+            circuit.TTL1b.set([len(signal)]) # Fire TTL1 at end of waveform
         circuit.trigger(mode) 
+        # Note that because circuit resets on TTL1 and TTL1 fires at the end of
+        # the waveform, the circuit will wrap to the beginnign of the waveform
+        # and start playing it out again.  If TTL1 fired halfway through the
+        # waveform, it would be truncated and the playout would start at the
+        # beginning.
 
+    # See cns.signal.type.* for the code that generates these waveforms
     while True:
         response = raw_input(textwrap.dedent(mesg))
         if response == 'q':
@@ -268,6 +251,20 @@ def main():
             mode = 4
         else:
             print "Invalid option"
+
+# Python files can either be imported as part of another program or executed
+# directly via the command line.  __name__ is a variable available to every
+# script that tells the script whether it is being launched directly or being
+# imported by another program.
+# 
+# This is a very neat trick for testing code.  If you are writing a module of
+# functions you can write some test or demo functions that are run whenever you
+# execute the file directly, but not when you import the functions for use by
+# another script.
+#
+# >>> if __name__ == '__main__':
+# >>>     run_demo_code()
+# >>>     run_test_code()
 
 if __name__ == '__main__':
     import cProfile
