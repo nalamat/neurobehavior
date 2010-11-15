84a741090ca708f8193dddd18b8a796d27d6db59
bburan <bburan@localhost>
2010-11-15 15:05:44 -0500

    Added shuffled set, doc enhancements and refactored existing choices to prevent them from modifying sequence order.

--- cns/choice.py
+++ cns/choice.py
@@ -1,39 +1,68 @@
-import numpy as np
-
-def copy_args(f):
-    def _inner(sequence):
-        f(sequence[:])
-    return _inner
+'''
+Each generator takes a sequence and returns a single element on each call.  The
+order of the elements returned depends on the algorithm.  The generators do not
+modify the sequence (if you must sort the sequence in a certain order, make a
+copy of it first).
+'''
 
 def ascending(sequence):
-    index = 0
-    sequence.sort()
+    '''
+    Returns elements from the sequence in ascending order.  When the last
+    element is reached, loop around to the beginning.
+    '''
+    from numpy import argsort
+    ordered_indices = argsort(sequence)
     while True:
-        yield sequence[index]
-        index = (index+1)%len(sequence)
+        for i in ordered_indices:
+            yield sequence[i]
 
 def descending(sequence):
-    index = 0
-    sequence.sort(reverse=True)
+    '''
+    Returns elements from the sequence in descending order.  When the last
+    element is reached, loop around to the beginning.
+    '''
+    from numpy import argsort
+    ordered_indices = argsort(sequence)
     while True:
-        yield sequence[index]
-        index = (index+1)%len(sequence)
+        for i in reversed(ordered_indices):
+            yield sequence[i]
 
 def pseudorandom(sequence):
+    '''
+    Returns a randomly selected element from the sequence.
+    '''
+    from numpy.random import randint
     while True:
-        index = np.random.randint(0, len(sequence))
-        yield sequence[index]
+        i = randint(0, len(sequence))
+        yield sequence[i]
 
 def exact_order(sequence):
-    index = 0
+    '''
+    Returns elements in the exact order they are provided.
+    '''
     while True:
-        yield sequence[index]
-        index = index+1%len(sequence)
+        for i in range(sequence):
+            yield sequence[i]
+
+def shuffled_set(sequence):
+    '''
+    Returns a randomly selected element from the sequence and removes it from
+    the sequence.  Once the sequence is exhausted, repopulate list with the
+    original sequence. 
+    '''
+    from numpy.random import shuffle
+    while True:
+        indices = range(len(sequence))
+        shuffle(indices) # Shuffle is in-place
+        for i in indices:
+            yield sequence[i]
 
 options = { 'ascending':    ascending,
             'descending':   descending,
             'pseudorandom': pseudorandom,
-            'exact order':  exact_order, }
+            'exact order':  exact_order, 
+            'shuffled set': shuffled_set,
+            }
 
 def get(type, sequence):
     try: len(sequence)
