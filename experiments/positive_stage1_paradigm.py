b44ac60ae1366134b637974a8e365cd29f1a0fdd
bburan <bburan@localhost>
2011-01-21 12:15:38 -0500

    Add control of attenuation to experiments.  This is a temporary feature until we get Sharad and Merri's calibration code.

--- experiments/positive_stage1_paradigm.py
+++ experiments/positive_stage1_paradigm.py
@@ -1,4 +1,4 @@
-from enthought.traits.api import DelegatesTo, Enum
+from enthought.traits.api import DelegatesTo, Enum, Range
 from enthought.traits.ui.api import View, VGroup, Item, EnumEditor, \
         InstanceEditor
 from abstract_experiment_paradigm import AbstractExperimentParadigm
@@ -7,11 +7,13 @@ from signals import signal_options
 
 class PositiveStage1Paradigm(AbstractExperimentParadigm, PumpParadigmMixin):
 
+    attenuation = Range(0.0, 120.0, 40)
     signal = DelegatesTo('selector', store='child')
     signal = Enum(signal_options.keys())
 
     traits_view = View(
             VGroup(
+                Item('attenuation', label='Attenuation (dB)', style='text'),
                 Item('signal', editor=EnumEditor(values=signal_options)),
                 Item('signal', editor=InstanceEditor(), style='custom'),
                 show_border=True, label='Signal'
