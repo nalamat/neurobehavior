3ec44c332d7917cb468c6edf45970856877ac806
Brad Buran <Brad Buran@localhost>
2011-03-10 15:35:42 -0500

    Improved logging configuration and error handling during experiment

---
+++ experiments/loader.py
@@ -0,0 +1,263 @@
+from enthought.etsconfig.api import ETSConfig
+ETSConfig.toolkit='qt4'
+
+from enthought.pyface.api import error, information
+from cns.data.ui.cohort import CohortView, CohortViewHandler
+from cns.data import persistence
+from cns.data.h5_utils import get_or_append_node
+import sys
+import tables
+from os.path import join
+
+from enthought.traits.api import Any, Trait, TraitError, Bool
+from enthought.traits.ui.api import View, Item, VGroup, HGroup, Spring
+
+import logging
+log = logging.getLogger(__name__)
+
+# Import the experiments
+from cns.data.ui.cohort import CohortEditor, CohortView, CohortViewHandler
+
+# Aversive
+from abstract_aversive_experiment import AbstractAversiveExperiment
+from aversive_data import RawAversiveData as AversiveData
+# Aversive FM
+from aversive_fm_paradigm import AversiveFMParadigm
+from aversive_fm_controller import AversiveFMController
+# Aversive AM Noise
+from aversive_am_noise_paradigm import AversiveAMNoiseParadigm
+from aversive_am_noise_controller import AversiveAMNoiseController
+
+# Positive
+from abstract_positive_experiment import AbstractPositiveExperiment
+from positive_data import PositiveData
+# Positive AM Noise
+from positive_am_noise_paradigm import PositiveAMNoiseParadigm
+from positive_am_noise_controller import PositiveAMNoiseController
+# Positive DT
+from positive_dt_experiment import PositiveDTExperiment
+from positive_dt_paradigm import PositiveDTParadigm
+from positive_dt_controller import PositiveDTController
+from positive_dt_data import PositiveDTData
+
+from cns.data.h5_utils import append_node, append_date_node
+
+class ExperimentCohortView(CohortView):
+
+    traits_view = View(
+        VGroup(
+            HGroup(
+                Item('object.cohort.description', style='readonly',
+                    springy=True),
+                show_border=True,
+                ),
+            Item('object.cohort.animals', editor=CohortEditor(),
+                 show_label=False, style='readonly'),
+        ),
+        title='Cohort',
+        height=400,
+        width=600,
+        resizable=True,
+    )
+
+class ExperimentLauncher(CohortViewHandler):
+
+    experiment      = Any
+    paradigm        = Any
+    controller      = Any
+    data            = Any
+    spool_physiology = Bool(True)
+
+    last_paradigm   = Trait(None, Any)
+
+    def init(self, info):
+        if not self.load_file(info):
+            sys.exit()
+
+    def launch_experiment(self, info, selected):
+        '''
+        Runs specified experiment type.  On successful completion of an
+        experiment, marks the animal as processed and saves the last paradigm
+        used.  If the experiment is launched but not run, changes to the
+        paradigm will not be saved.
+        '''
+
+        try:
+            item = selected
+            if item.store_node._v_isopen:
+                animal_node = item.store_node
+            else:
+                log.debug('Opening node %r for %r', item.store_node, item)
+                file = tables.openFile(item.store_node_source, 'a',
+                                       rootUEP=item.store_node_path)
+                animal_node = file.root
+            
+            # Try to load settings from the last time the subject was run.  If
+            # we cannot load the settings for whatever reason, notify the user
+            # and fall back to the default settings.
+            store_node = get_or_append_node(animal_node, 'experiments')
+            paradigm_node = get_or_append_node(store_node, 'last_paradigm')
+            paradigm_name = self.experiment.__name__
+            paradigm = None
+            try:
+                paradigm = persistence.load_object(paradigm_node, paradigm_name)
+            except tables.NoSuchNodeError:
+                mesg = 'No prior paradigm found.  Creating new paradigm.'
+                log.debug(mesg)
+                information(info.ui.control, mesg)
+            except TraitError, e:
+                mesg = 'Unable to load prior settings.  Creating new paradigm.'
+                log.debug(mesg)
+                log.exception(e)
+                error(info.ui.control, mesg)
+            except ImportError, e:
+                mesg = 'Unable to load prior settings.  Creating new paradigm.'
+                log.debug(mesg)
+                log.exception(e)
+                error(info.ui.control, mesg)
+            except persistence.PersistenceReadError, e:
+                mesg = 'Unable to load prior settings.  Creating new paradigm.'
+                log.debug(mesg)
+                log.exception(e)
+                error(info.ui.control, mesg)
+                
+            pre = self.__class__.__name__ + '_'
+            exp_node = append_date_node(store_node, pre)
+            data_node = append_node(exp_node, 'data')
+
+            model = self.experiment(
+                    store_node=store_node, 
+                    exp_node=exp_node,
+                    data_node=data_node, 
+                    animal=item,
+                    data=self.data(store_node=data_node),
+                    paradigm=self.paradigm(),
+                    spool_physiology=self.spool_physiology,
+                    )
+            
+            try:
+                if paradigm is not None:
+                    log.debug('Using paradigm from last time this animal was run')
+                    model.paradigm = paradigm
+                elif self.last_paradigm is not None:
+                    log.debug('Animal does not have any prior paradigm.')
+                    log.debug('Using paradigm from previous animal.')
+                    model.paradigm = self.last_paradigm
+            except TraitError:
+                log.debug('Prior paradigm is not compatible with experiment')
+    
+            if self.controller is not None:
+                ui = model.edit_traits(parent=info.ui.control, kind='livemodal',
+                        handler=self.controller())
+            else:
+                ui = model.edit_traits(parent=info.ui.control, kind='livemodal')
+
+            if ui.result:
+                persistence.add_or_update_object(model.paradigm, paradigm_node,
+                        paradigm_name)
+                item.processed = True
+                self.last_paradigm = model.paradigm
+            
+            # Close the file if we opened it
+            try: 
+                file.close()
+            except NameError: 
+                pass
+            
+        except AttributeError, e: 
+            log.exception(e)
+        except SystemError, e:
+            from textwrap import dedent
+            mesg = """\
+            Could not launch experiment.  This likely means that you forgot to
+            turn on a piece of equipment.  Please check and ensure that the RX6,
+            RZ5 and PA5 are turned on.  If you still get this error message,
+            please try power-cycling the rack.  If this still does not fix the
+            error, power-cycle the computer as well.  
+            
+            Please remember that you need to give the equipment rack a minute to
+            boot up once you turn it back on before attempting to launch an
+            experiment.
+            """
+            error(info.ui.control, str(e) + '\n\n' + dedent(mesg))
+
+    def object_dclicked_changed(self, info):
+        if info.initialized:
+            self.launch_experiment(info, info.object.selected)
+
+def test_experiment(etype, spool_physiology):
+    from cns import TEMP_ROOT
+    filename = join(TEMP_ROOT, 'test_experiment.hd5')
+    file = tables.openFile(filename, 'w')
+    store_node = file.root
+
+    classes = EXPERIMENTS[etype]
+    experiment_class = classes[0]
+    paradigm_class = classes[1]
+    controller_class = classes[2]
+    data_class = classes[3]
+
+    pre = experiment_class.__class__.__name__ + '_'
+    exp_node = append_date_node(store_node, pre)
+    data_node = append_node(exp_node, 'data')
+
+    model = experiment_class(
+            store_node=store_node, 
+            exp_node=exp_node,
+            data_node=data_node, 
+            data=data_class(store_node=data_node),
+            paradigm=paradigm_class(),
+            spool_physiology=spool_physiology,
+            )
+    model.configure_traits(handler=controller_class())
+
+def profile_experiment(etype, spool_physiology=False):
+    from cns import TEMP_ROOT
+    import cProfile
+    profile_data_file = join(TEMP_ROOT, 'profile.dmp')
+    cProfile.run('test_experiment("%s", %s)' % (etype, spool_physiology),
+            profile_data_file)
+
+    # Once experiment is done, print out some statistics
+    import pstats
+    p = pstats.Stats(profile_data_file)
+    p.strip_dirs().sort_stats('cumulative').print_stats(50)
+
+def launch_experiment(etype, spool_physiology=False, profile=False):
+    classes = EXPERIMENTS[etype]
+    experiment_class = classes[0]
+    paradigm_class = classes[1]
+    controller_class = classes[2]
+    data_class = classes[3]
+
+    handler = ExperimentLauncher(experiment=experiment_class,
+            paradigm=paradigm_class, controller=controller_class,
+            data=data_class, spool_physiology=spool_physiology)
+    ExperimentCohortView().configure_traits(handler=handler)
+
+EXPERIMENTS = {
+        'positive_am_noise': (
+            AbstractPositiveExperiment, 
+            PositiveAMNoiseParadigm,
+            PositiveAMNoiseController, 
+            PositiveData,
+            ),
+        'positive_dt': (
+            PositiveDTExperiment, 
+            PositiveDTParadigm,
+            PositiveDTController, 
+            PositiveDTData,
+            ),
+        'aversive_fm': (
+            AbstractAversiveExperiment, 
+            AversiveFMParadigm,
+            AversiveFMController, 
+            AversiveData,
+            ),
+        'aversive_am_noise': (
+            AbstractAversiveExperiment, 
+            AversiveFMParadigm,
+            AversiveFMController, 
+            AversiveData,
+            ),
+        }
---
+++ experiments/positive_data.py
@@ -0,0 +1,323 @@
+from __future__ import division
+
+from abstract_experiment_data import AbstractExperimentData
+from sdt_data_mixin import SDTDataMixin
+from cns.channel import FileChannel
+from enthought.traits.api import Instance, List, CFloat, Int, Float, Any, \
+    Range, DelegatesTo, cached_property, on_trait_change, Array, Event, \
+    Property, Undefined, Callable, Str, Enum, Bool, Int, Str, Tuple
+from datetime import datetime
+import numpy as np
+from cns.data.h5_utils import append_node, get_or_append_node
+from cns.pipeline import deinterleave, broadcast
+
+from cns.channel import Timeseries
+
+
+from enthought.chaco.api import AbstractPlotData
+
+import logging
+log = logging.getLogger(__name__)
+
+# Score functions
+ts = lambda TTL: np.flatnonzero(TTL)
+edge_rising = lambda TTL: np.r_[0, np.diff(TTL.astype('i'))] == 1
+edge_falling = lambda TTL: np.r_[0, np.diff(TTL.astype('i'))] == -1
+
+def apply_mask(fun, seq, mask):
+    seq = np.array(seq).ravel()
+    return [fun(seq[m]) for m in mask]
+
+LOG_DTYPE = [('timestamp', 'i'), ('name', 'S64'), ('value', 'S128'), ]
+
+class PositiveData_0_1(AbstractExperimentData, SDTDataMixin, AbstractPlotData):
+
+    def get_data(self, name):
+        return getattr(self, name)
+
+    store_node = Any
+
+    contact_data = Any
+
+    poke_TTL = Instance(FileChannel, 
+            store='channel', store_path='contact/poke_TTL')
+    spout_TTL = Instance(FileChannel, 
+            store='channel', store_path='contact/spout_TTL')
+    trial_TTL = Instance(FileChannel, 
+            store='channel', store_path='contact/trial_TTL')
+    response_TTL = Instance(FileChannel, 
+            store='channel', store_path='contact/response_TTL')
+    pump_TTL = Instance(FileChannel,
+            store='channel', store_path='contact/pump_TTL')
+    signal_TTL = Instance(FileChannel,
+            store='channel', store_path='contact/signal_TTL')
+    reaction_TTL = Instance(FileChannel,
+            store='channel', store_path='contact/reaction_TTL')
+    reward_TTL = Instance(FileChannel,
+            store='channel', store_path='contact/reward_TTL')
+
+    trial_start_timestamp = Instance('cns.channel.Timeseries', ())
+    trial_end_timestamp = Instance('cns.channel.Timeseries', ())
+    timeout_start_timestamp = Instance('cns.channel.Timeseries', ())
+    timeout_end_timestamp = Instance('cns.channel.Timeseries', ())
+
+    def _create_channel(self, name, dtype):
+        contact_node = get_or_append_node(self.store_node, 'contact')
+        return FileChannel(node=contact_node, name=name, dtype=dtype)
+
+    def _poke_TTL_default(self):
+        return self._create_channel('poke_TTL', np.bool)
+
+    def _spout_TTL_default(self):
+        return self._create_channel('spout_TTL', np.bool)
+
+    def _trial_TTL_default(self):
+        return self._create_channel('trial_TTL', np.bool)
+
+    def _reaction_TTL_default(self):
+        return self._create_channel('reaction_TTL', np.bool)
+
+    def _pump_TTL_default(self):
+        return self._create_channel('pump_TTL', np.bool)
+
+    def _signal_TTL_default(self):
+        return self._create_channel('signal_TTL', np.bool)
+
+    def _response_TTL_default(self):
+        return self._create_channel('response_TTL', np.bool)
+
+    def _reward_TTL_default(self):
+        return self._create_channel('reward_TTL', np.bool)
+
+    TRIAL_DTYPE = [('parameter', 'f'), ('ts_start', 'i'), ('ts_end', 'i'),
+                   ('type', 'S4'), ('response', 'S16'), ('reaction_time', 'f')]
+
+    trial_log = List(Tuple(Float, Int, Int, Str, Str, Float), store='table',
+                     dtype=TRIAL_DTYPE)
+
+    # Total GO trials presented
+    num_go = Int(store='attribute')
+    # Total GO trials where subject provided response
+    num_go_response = Int(store='attribute')
+    # Total NOGO trials presented
+    num_nogo = Int(store='attribute')
+    # Total NOGO trials where subject provided response
+    num_nogo_response = Int(store='attribute')
+    # Total false alarms
+    num_fa = Int(store='attribute')
+    # Total hits
+    num_hit = Int(store='attribute')
+    # num_fa/num_nogo
+    fa_frac = Float(store='attribute')
+    # num_hit/num_go
+    hit_frac = Float(store='attribute')
+    # num_fa/num_nogo_response
+    response_fa_frac = Float(store='attribute')
+    # num_hit/num_go_response
+    response_hit_frac = Float(store='attribute')
+
+    def log_trial(self, ts_start, ts_end, ttype, parameter):
+        log.debug('Logging trial %s (%d, %d)', ttype, ts_start, ts_end)
+
+        ts_start = int(ts_start)
+        ts_end = int(ts_end)
+
+        poke_data = self.poke_TTL.get_range_index(ts_start, ts_end)
+        spout_data = self.spout_TTL.get_range_index(ts_start, ts_end)
+        if poke_data.all():
+            response = 'no withdraw'
+        elif spout_data.any():
+            response = 'spout'
+        elif poke_data[-1] == 1:
+            response = 'poke'
+        else:
+            response = 'no response'
+
+        try:
+            if response == 'spout':
+                reaction_time = ts(edge_rising(spout_data))[0]/self.spout_TTL.fs
+            elif response == 'poke':
+                reaction_time = ts(edge_rising(poke_data))[0]/self.poke_TTL.fs
+            else:
+                reaction_time = -1
+        except:
+            reaction_time = -1
+            
+        data = parameter, ts_start, ts_end, ttype, response, reaction_time
+        self.trial_log.append(data)
+
+    ts_seq       = Property(Array('i'), depends_on='trial_log')
+    par_seq      = Property(Array('f'), depends_on='trial_log')
+    ttype_seq    = Property(Array('S'), depends_on='trial_log')
+    resp_seq     = Property(Array('S'), depends_on='trial_log')
+
+    go_indices   = Property(Array('i'), depends_on='ttype_seq')
+    nogo_indices = Property(Array('i'), depends_on='ttype_seq')
+
+    par_go_mask = Property(List(Array('b')), depends_on='trial_log')
+    par_nogo_mask = Property(List(Array('b')), depends_on='trial_log')
+    par_spout_mask = Property(List(Array('b')), depends_on='trial_log')
+    par_poke_mask = Property(List(Array('b')), depends_on='trial_log')
+
+    par_go_count = Property(List(Int), depends_on='trial_log')
+    par_nogo_count = Property(List(Int), depends_on='trial_log')
+    par_hit_count = Property(List(Int), depends_on='trial_log')
+    par_fa_count = Property(List(Int), depends_on='trial_log')
+
+    par_hit_frac = Property(List(Float), depends_on='trial_log')
+    par_fa_frac = Property(List(Float), depends_on='trial_log')
+    
+    global_fa_frac = Property(Float, depends_on='trial_log')
+
+    pars = Property(List(Int), depends_on='trial_log')
+    go_trial_count = Property(Int, store='attribute', depends_on='trial_log')
+    nogo_trial_count = Property(Int, store='attribute', depends_on='trial_log')
+
+    PAR_INFO_DTYPE = [
+            ('par', 'f'), 
+            ('nogo_count', 'i'),
+            ('go_count', 'i'),
+            ('fa_count', 'i'),
+            ('hit_count', 'i'),
+            ('hit_frac', 'f'),
+            ('fa_frac', 'f'),
+            ('d', 'f'),
+            ]
+    par_info = Property(store='table', dtype=PAR_INFO_DTYPE)
+
+    def _get_par_info(self):
+        return zip(self.pars, self.par_nogo_count, self.par_go_count,
+                self.par_fa_count, self.par_hit_count, self.par_fa_frac,
+                self.par_hit_frac, self.par_dprime)
+
+    @cached_property
+    def _get_ts_seq(self):
+        return [t[1] for t in self.trial_log]
+
+    @cached_property
+    def _get_par_seq(self):
+        return [t[0] for t in self.trial_log]
+
+    @cached_property
+    def _get_ttype_seq(self):
+        return [t[3] for t in self.trial_log]
+
+    @cached_property
+    def _get_resp_seq(self):
+        return [t[4] for t in self.trial_log]
+
+    def _get_indices(self, ttype):
+        if len(self.ttype_seq):
+            mask = np.asarray(self.ttype_seq)==ttype
+            return np.flatnonzero(mask)
+        else:
+            return []
+
+    @cached_property
+    def _get_go_indices(self):
+        return self._get_indices('GO')
+
+    @cached_property
+    def _get_nogo_indices(self):
+        return self._get_indices('NOGO')
+
+    def _get_par_mask(self, sequence, value):
+        value_mask = np.array(sequence) == value
+        result = []
+        for par in self.pars:
+            par_mask = np.equal(self.par_seq, par)
+            if par_mask.ndim != 1:
+                par_mask = par_mask.all(axis=-1)
+            result.append(par_mask & value_mask)
+        return result
+
+    @cached_property
+    def _get_par_nogo_mask(self):
+        return self._get_par_mask(self.ttype_seq, 'NOGO')
+
+    @cached_property
+    def _get_par_go_mask(self):
+        return self._get_par_mask(self.ttype_seq, 'GO')
+
+    @cached_property
+    def _get_par_spout_mask(self):
+        return self._get_par_mask(self.resp_seq, 'spout')
+
+    @cached_property
+    def _get_par_poke_mask(self):
+        return self._get_par_mask(self.resp_seq, 'poke')
+
+    @cached_property
+    def _get_par_go_count(self):
+        return apply_mask(len, self.par_seq, self.par_go_mask)
+
+    @cached_property
+    def _get_par_nogo_count(self):
+        return apply_mask(len, self.par_seq, self.par_nogo_mask)
+
+    @cached_property
+    def _get_par_hit_count(self):
+        mask = [go & spout for go, spout in \
+                zip(self.par_go_mask, self.par_spout_mask)]
+        return apply_mask(len, self.par_seq, mask)
+
+    @cached_property
+    def _get_par_fa_count(self):
+        mask = [nogo & spout for nogo, spout in \
+                zip(self.par_nogo_mask, self.par_spout_mask)]
+        return apply_mask(len, self.par_seq, mask)
+
+    @cached_property
+    def _get_pars(self):
+        # We only want to return pars for complete trials (e.g. ones for which a
+        # go was presented).
+        seq = np.take(self.par_seq, self.go_indices, axis=0)
+
+        try:
+            # This should work for "complex" parameters where we are varying the
+            # signal across more than one dimension.
+            unique = set([tuple(i) for i in seq])
+        except:
+            unique = set(seq)
+        return sorted(unique)
+
+    @cached_property
+    def _get_par_hit_frac(self):
+        return np.true_divide(self.par_hit_count, self.par_go_count)
+
+    @cached_property
+    def _get_par_fa_frac(self):
+        return np.true_divide(self.par_fa_count, self.par_nogo_count)
+
+    @cached_property
+    def _get_global_fa_frac(self):
+        nogo_count = len(self.nogo_indices)
+
+        if nogo_count == 0:
+            return np.nan
+
+        nogo_resp = np.take(self.resp_seq, self.nogo_indices)
+        # fa_mask is a boolean array where 1 indicates the subject went to the
+        # spout.  We can simply compute the sum of this array to determien how
+        # many times the subject went to the spout (e.g. "false alarmed")
+        fa_mask = nogo_resp == 'spout'
+        fa_count = np.sum(fa_mask)
+
+        return float(fa_count)/float(nogo_count)
+
+    @cached_property
+    def _get_go_trial_count(self):
+        return len(self.go_indices)
+
+    @cached_property
+    def _get_nogo_trial_count(self):
+        return len(self.nogo_indices)
+
+    @on_trait_change('par_dprime')
+    def fire_data_changed(self):
+        self.data_changed = True
+
+PositiveData = PositiveData_0_1
+
+if __name__ == '__main__':
+    pass
