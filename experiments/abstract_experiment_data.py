fd812d54c530fb1859fcc3eeaba4154952c79cba
Brad Buran <Brad Buran@localhost>
2011-03-24 14:15:27 -0400

    Fixed event logging

--- experiments/abstract_experiment_data.py
+++ experiments/abstract_experiment_data.py
@@ -1,7 +1,16 @@
 from physiology_data_mixin import PhysiologyDataMixin
 from pump_data_mixin import PumpDataMixin
 
+from enthought.traits.api import List
+
+EVENT_DTYPE = [('timestamp', 'i'), ('name', 'S64'), ('value', 'S128'), ]
+
 class AbstractExperimentData(PhysiologyDataMixin, PumpDataMixin):
+
+    event_log = List(store='table', dtype=EVENT_DTYPE)
+
+    def log_event(self, timestamp, name, value):
+        self.event_log.append((timestamp, name, repr(value)))
 
     def channel_from_buffer(self, buffer, channel_name):
         channel_trait = self.trait(channel_name)
