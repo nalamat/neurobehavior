3c4190379a9843718371e78d6828d2c3b61b54b9
bburan <bburan@localhost>
2011-06-01 16:38:31 -0400

    Work on offline data analysis code

---
+++ cns/chaco/timeseries_plot.py
@@ -0,0 +1,96 @@
+import numpy as np
+
+from enthought.chaco.api import BaseXYPlot
+from enthought.enable.api import black_color_trait, LineStyle
+from enthought.traits.api import Instance, Float, Event, Bool, Enum, \
+        on_trait_change, Str
+
+class TimeseriesPlot(BaseXYPlot):
+    '''
+    Designed for efficiently handling time series data stored in a channel.
+    Each time a Channel.updated event is fired, the new data is obtained and
+    plotted.
+    '''
+
+    series                  = Instance('cns.channel.Timeseries')
+
+    fill_color              = black_color_trait
+    line_color              = black_color_trait
+    line_width              = Float(1.0)
+    line_style              = LineStyle
+    reference               = Float(-1.0)
+    data_changed            = Event
+    _data_cache_valid       = Bool(False)
+    _screen_cache_valid     = Bool(False)
+    rect_height             = Float(0.5)
+    rect_center             = Float(0.5)
+    label                   = Str("Timeseries")
+    text_rotation           = Float(np.pi/4)
+
+    def _gather_points(self):
+        if not self._data_cache_valid:
+            range = self.index_mapper.range
+            self._cached_data = self.series.get_range(range.low, range.high)
+            self._data_cache_valid = True
+            self._screen_cache_valid = False
+
+    def _get_screen_points(self):
+        if not self._screen_cache_valid:
+            # Obtain cached data bounds and create index points
+            t_screen = self.index_mapper.map_screen(self._cached_data)
+            self._cached_screen_index = t_screen
+            # Screen cache is valid
+            self._screen_cache_valid = True
+            
+        return self._cached_screen_index
+
+    def _draw_plot(self, gc, view_bounds=None, mode="normal"):
+        self._gather_points()
+        points = self._get_screen_points()
+        self._render(gc, points)
+
+    def _render(self, gc, points):
+        lines = points
+        n = len(lines)
+        if n == 0:
+            return
+
+        low = self.rect_center-self.rect_height*0.5
+        high = self.rect_center+self.rect_height*0.5
+        screen_low = self.value_mapper.map_screen(high)
+        screen_high = self.value_mapper.map_screen(low)
+
+        starts = np.column_stack((lines, np.ones(n)*screen_low))
+        ends = np.column_stack((lines, np.ones(n)*screen_high))
+
+        gc.save_state()
+        try:
+            gc.set_antialias(True)
+            gc.clip_to_rect(self.x, self.y, self.width, self.height)
+
+            # Set up appearance
+            gc.set_stroke_color(self.line_color_)
+            gc.set_fill_color(self.fill_color_)
+            gc.set_line_width(self.line_width) 
+            gc.set_line_dash(self.line_style_)
+            gc.set_line_join(0) # Curved
+
+            gc.begin_path()
+            gc.line_set(starts, ends)
+            for i, (x, y) in enumerate(starts):
+                gc.draw_marker_at_points([[x, y]], 1, 1)
+            gc.draw_path()
+            self._draw_default_axes(gc)
+        finally:
+            gc.restore_state()
+
+    def _data_changed(self):
+        self.invalidate_draw()
+        self._data_cache_valid = False
+        self.request_redraw()
+
+    def _series_changed(self, old, new):
+        if old is not None:
+            old.on_trait_change(self._data_changed, "updated", remove=True)
+        if new is not None:
+            new.on_trait_change(self._data_changed, "updated", dispatch="new")
---
+++ experiments/physiology_controller_mixin.py
@@ -0,0 +1,88 @@
+from enthought.traits.api import HasTraits, Instance, Any
+from enthought.traits.ui.api import HGroup, View, Item
+from tdt import DSPProcess
+from cns import RCX_ROOT
+from os.path import join
+
+class PhysiologyControllerMixin(HasTraits):
+
+    # By convention, all mixin classes should prepend attribute names with the
+    # mixin name (e.g. physiology).  This prevents potential namespace
+    # collisions.
+    iface_physiology        = Any
+    buffer_physiology_raw   = Any
+    buffer_physiology_proc  = Any
+    buffer_physiology_ts    = Any
+
+    def setup_physiology(self):
+        # Load the circuit
+        circuit = join(RCX_ROOT, 'physiology')
+        self.iface_physiology = self.process.load_circuit(circuit, 'RZ5')
+
+        # Initialize the buffers that will be spooling the data
+        self.buffer_physiology_raw = self.iface_physiology.get_buffer('craw',
+                'r', src_type='int16', dest_type='float32', channels=16) 
+        self.buffer_physiology_filt = self.iface_physiology.get_buffer('cfilt',
+                'r', src_type='int16', dest_type='float32', channels=16) 
+        self.buffer_physiology_ts = self.iface_physiology.get_buffer('trig/',
+                'r', src_type='int32', dest_type='int32', block_size=1)
+
+        # Ensure that the data store has the correct sampling frequency
+        self.model.data.physiology_raw.fs = self.buffer_physiology_raw.fs
+        self.model.data.physiology_processed.fs = self.buffer_physiology_filt.fs
+        self.model.data.physiology_ram.fs = self.buffer_physiology_raw.fs
+        self.model.data.physiology_ts.fs = self.buffer_physiology_ts.fs
+
+    def monitor_physiology(self):
+        # Acquire raw physiology data
+        waveform = self.buffer_physiology_raw.read()
+        self.model.data.physiology_raw.send(waveform)
+
+        # Acquire filtered physiology data
+        waveform = self.buffer_physiology_filt.read()
+        self.model.data.physiology_processed.send(waveform)
+
+        # We also send the processed data to a memory buffer for display in the
+        # plotting.  It's very slow when the plot has to re-extract the data
+        # from the file and we'd like to avoid this.
+        # self.model.data.physiology_ram.send(waveform)
+
+        # Get the timestamps
+        ts = self.buffer_physiology_ts.read()
+        self.model.data.physiology_ts.send(ts)
+
+    def set_monitor_fc_highpass(self, value):
+        self.iface_physiology.set_tag('FiltHP', value)
+
+    def set_monitor_fc_lowpass(self, value):
+        self.iface_physiology.set_tag('FiltLP', value)
+
+    def set_monitor_ch_1(self, value):
+        self.iface_physiology.set_tag('ch1_out', value)
+
+    def set_monitor_ch_2(self, value):
+        self.iface_physiology.set_tag('ch2_out', value)
+
+    def set_monitor_ch_3(self, value):
+        self.iface_physiology.set_tag('ch3_out', value)
+
+    def set_monitor_ch_4(self, value):
+        self.iface_physiology.set_tag('ch4_out', value)
+
+    def set_monitor_gain_1(self, value):
+        self.iface_physiology.set_tag('ch1_out_sf', value*1e3)
+
+    def set_monitor_gain_2(self, value):
+        self.iface_physiology.set_tag('ch2_out_sf', value*1e3)
+
+    def set_monitor_gain_3(self, value):
+        self.iface_physiology.set_tag('ch3_out_sf', value*1e3)
+
+    def set_monitor_gain_4(self, value):
+        self.iface_physiology.set_tag('ch4_out_sf', value*1e3)
+
+    def set_visible_channels(self, value):
+        self.model.physiology_plot.visible = value
+
+    def set_diff_matrix(self, value):
+        self.iface_physiology.set_coefficients('diff_map', value.ravel())
