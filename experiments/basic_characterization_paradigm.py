d8ae111bc4f3581acd1251289c9024f288aa6ae1
Brad Buran <Brad Buran@localhost>
2011-08-10 18:32:41 -0400

    More sensible ranges for the basic characterization signal options
    * Bandwidth should be a range from 0 to 2, not 0 to 16
    * Token duration and trial duration should never be 0
    * Removed commutator inhibit because this should be handled by the physiology
      acquisition controller

--- experiments/basic_characterization_paradigm.py
+++ experiments/basic_characterization_paradigm.py
@@ -30,19 +30,15 @@ class BasicCharacterizationParadigm(AbstractExperimentParadigm):
     primary_attenuation     = Range(0, 120, 120, init=True, immediate=True)
     secondary_attenuation   = Range(0, 120, 120, init=True, immediate=True)
 
-    token_duration          = Range(0.0, 10.0, 1.0, init=True, immediate=True)
-    trial_duration          = Range(0.0, 20.0, 2.0, init=True, immediate=True)
-
-    center_frequency        = Range(0.0, 50e3, 5e3, init=True, immediate=True)
-    bandwidth_ratio         = Range(0.0, 16, 0.3, init=True, immediate=True)
+    token_duration          = Range(0.01, 10.0, 1.0, init=True, immediate=True)
+    trial_duration          = Range(0.01, 20.0, 2.0, init=True, immediate=True)
+    center_frequency        = Range(100, 50e3, 5e3, init=True, immediate=True)
+    bandwidth_ratio         = Range(0.0, 2, 0.3, init=True, immediate=True)
     modulation_frequency    = Range(0.0, 100, 5.0, init=True, immediate=True)
     modulation_depth        = Range(0.0, 1.0, 0.0, init=True, immediate=True)
 
-    commutator_inhibit      = Bool(False, init=True, immediate=True)
-
     traits_view = View(
             VGroup(
-                'commutator_inhibit',
                 HGroup(
                     Item('mute_speakers', show_label=False),
                     Item('swap_speakers', show_label=False),
