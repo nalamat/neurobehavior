f8f36a82e31e64c0dc8a8e38e79896cda645deb2
brad <brad@phoenix>
2011-08-30 17:46:36 -0400

    Fixing some errors introduced when using the auto-reconcile during the merge

--- experiments/am_noise_paradigm_mixin.py
+++ experiments/am_noise_paradigm_mixin.py
@@ -5,8 +5,8 @@ from evaluate import Expression
 class AMNoiseParadigmMixin(HasTraits):
     
     kw = {'context': True, 'store': 'attribute', 'log': True}
-    fc = ExpressionTrait(3e3, label='Center frequency (Hz)')
-    modulation_onset = ExpressionTrait('uniform(0.5, 1.0)', label='Modulation onset (s)')
+    fc = Expression(3e3, label='Center frequency (Hz)', **kw)
+    modulation_onset = Expression('uniform(0.2, 0.4)', label='Modulation onset (s)')
     duration = Expression(1.2, label='Signal duration (s)', **kw)
     rise_fall_time = Expression(0.15, label='Ramp time (s)', **kw)
     fm = Expression(5, label='Modulation frequency (Hz)', **kw)
@@ -17,19 +17,20 @@ class AMNoiseParadigmMixin(HasTraits):
     modulation_depth = Expression(1.0, label='Modulation depth (frac)', **kw)
     reaction_window_delay = Expression("modulation_onset+0.25",
                                        label='Reaction window delay (s)', **kw)
-    
-    bandwidth = ExpressionTrait(3e3, label='Bandwidth (Hz)')
-    rs = ExpressionTrait(60, label='Minimum attenuation in stop band (dB)')
-    rp = ExpressionTrait(4, label='Maximum ripple in pass band (dB)')
-    order = ExpressionTrait(1, label='Filter order')
+    bandwidth = Expression(3e3, label='Bandwidth (Hz)', **kw)
+    rs = Expression(60, label='Minimum attenuation in stop band (dB)', **kw)
+    rp = Expression(4, label='Maximum ripple in pass band (dB)', **kw)
+    order = Expression(1, label='Filter order', **kw)
 
     # This defines what is visible via the GUI
+    am_noise_group = VGroup(
             'duration',
             'rise_fall_time',
             'fm',
             'fc',
             'modulation_depth',
             'modulation_onset',
+            'level',
             VGroup(
                 'seed',
                 'bandwidth',
