f4968431d80140ca4612df778e535e474b489717
bburan <bburan@localhost>
2011-09-19 21:23:41 -0400

    Modified processed and spikes attributes to save data to a temporary file
    After discussing with DHS and GvT, it has become clear that we are only using
    the raw waveform, ts and epoch information for our data analysis.  We don't use
    the processed and spike data because we sometimes change the filter, reference
    and spike threhsold settings during an experiment, so these data are unreliable.
    Removing these data has reduced file size by 50%.
    
    Furthermore, I have enabled compression for the raw data, which should give us
    an additional 50% reduction in file size.  I had this disabled before due to the
    additional CPU overhead this generates; however, since we don't typically plot
    the raw waveform, the compression should have very little effect.  I also
    turned on checksumming as well to ensure integrity of the data.

--- experiments/physiology_data.py
+++ experiments/physiology_data.py
@@ -2,7 +2,6 @@ from cns import get_config
 from enthought.traits.api import HasTraits, Instance, List, Any
 from cns.channel import (FileMultiChannel, FileChannel, FileSnippetChannel,
         FileTimeseries, FileEpoch)
-from cns.data.h5_utils import get_or_append_node
 import numpy as np
 
 CHANNELS = get_config('PHYSIOLOGY_CHANNELS')
@@ -10,40 +9,52 @@ CHANNELS = get_config('PHYSIOLOGY_CHANNELS')
 class PhysiologyData(HasTraits):
 
     store_node = Any
+    temp_node = Any
 
-    # Raw physiology data
-    raw = Instance(FileMultiChannel, store='channel')
+    def _temp_node_default(self):
+        import tables
+        from tempfile import mkdtemp
+        from os import path
+        filename = path.join(mkdtemp(), 'processed_physiology.h5')
+        tempfile = tables.openFile(filename, 'w')
+        return tempfile.root
 
-    # Data after it has been referenced to a differential and band-pass filtered
-    processed = Instance(FileMultiChannel, store='channel')
-    sweep = Instance(FileChannel, store='channel')
-    ts = Instance(FileTimeseries)
-    epoch = Instance(FileEpoch)
-    spikes = List(Instance(FileSnippetChannel))
+    # Raw (permanent) physiology data that will be stored in the data file
+    raw         = Instance(FileMultiChannel, store='channel')
+    sweep       = Instance(FileChannel, store='channel')
+    ts          = Instance(FileTimeseries)
+    epoch       = Instance(FileEpoch)
 
-    def _spikes_default(self):
-        channels = []
-        node = self.store_node
-        for i in range(CHANNELS):
-            name = 'spike_{:02}'.format(i+1)
-            ch = FileSnippetChannel(node=node, name=name, dtype=np.float32,
-                                    snippet_size=18)
-            channels.append(ch)
-        return channels
+    # Temporary data for plotting.  Stored in a temporary file that will
+    # eventually be discarded at the end of the experiment.
+    processed   = Instance(FileMultiChannel, store='channel')
+    spikes      = List(Instance(FileSnippetChannel))
 
     def _sweep_default(self):
-        return FileChannel(node=self.store_node, name='sweep', dtype=np.bool)
+        return FileChannel(node=self.store_node, name='sweep', dtype=np.bool,
+                use_checksum=True)
 
     def _raw_default(self):
         return FileMultiChannel(node=self.store_node, channels=CHANNELS,
-                name='raw', dtype=np.float32)
-
-    def _processed_default(self):
-        return FileMultiChannel(node=self.store_node, channels=CHANNELS,
-                name='processed', dtype=np.float32)
+                name='raw', dtype=np.float32, compression_type='lzo',
+                compression_level=1, use_shuffle=True, use_checksum=True)
 
     def _ts_default(self):
-        return FileTimeseries(node=self.store_node, name='ts', dtype=np.int32)
+        return FileTimeseries(node=self.store_node, name='ts', dtype=np.int32,
+                use_checksum=True)
 
     def _epoch_default(self):
         return FileEpoch(node=self.store_node, name='epoch', dtype=np.int32)
+
+    def _processed_default(self):
+        return FileMultiChannel(node=self.temp_node, channels=CHANNELS,
+                name='processed', dtype=np.float32)
+
+    def _spikes_default(self):
+        channels = []
+        for i in range(CHANNELS):
+            name = 'spike_{:02}'.format(i+1)
+            ch = FileSnippetChannel(node=self.temp_node, name=name,
+                    dtype=np.float32, snippet_size=18)
+            channels.append(ch)
+        return channels
