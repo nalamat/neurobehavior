54b5df06e0ceb18f0b6d70cf44c890aa65d5c369
sanesadmin <sanesadmin@shopadmin-PC.nyu.edu>
2011-10-06 17:42:08 -0400

    Removed obsolete demos and tried to get remaining demos working

--- demo/animal_table.py
+++ demo/animal_table.py
@@ -1,9 +1,13 @@
-from cns.data.view import CohortEditDialog
+from cns.data.ui.cohort import CohortView
 from cns.data.type import Cohort, Animal
+from datetime import date, timedelta
 
-c = Cohort(description='Control Group 1',
-           animals=[Animal(parents='HH', identifier='tail'),
-                    Animal(parents='HH', identifier='fluffy'),])
-            
-CohortEditDialog().configure_traits()
-CohortEditDialog(cohort=c, editable=False).configure_traits()
+birth = date.today()-timedelta(days=30)
+
+animals = [Animal(parents='HH', identifier='tail', birth=birth, sex='F'),
+	   Animal(parents='HH', identifier='fluffy', birth=birth, sex='M'),
+	   Animal(parents='UU', identifier='blue', birth=date.today()),
+	  ]
+
+cohort = Cohort(description='Control Group 1', animals=animals)
+CohortView(cohort=cohort).configure_traits(view='edit_view')
