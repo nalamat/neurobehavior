50f23b6fe9b13e6ace14ebaeb9da873f3a2917ac
bburan <bburan@localhost>
2012-02-04 17:52:33 -0500

    Additional work to unwind dependency on Neurogen
    Did not test to see if this is a stable, working revision (needed to commit the
    changes so I could update to the parent of this revision for Ramanjot's
    experiment).

--- launchers/aversive_am_noise_cl.py
+++ launchers/aversive_am_noise_cl.py
@@ -3,9 +3,8 @@ from enthought.traits.ui.api import View, Include, VGroup, Item, Include
 from enthought.enable.api import Component, ComponentEditor
 from experiments.evaluate import Expression
 
-import neurogen.block_definitions as blocks
-from neurogen.sink import Sink
 import numpy as np
+from cns import signal
 
 from experiments import (
         # Controller and mixins
@@ -37,78 +36,51 @@ class Controller(
     def initial_setting(self):
         return self.nogo_setting()
 
-    int_carrier     = Instance(blocks.Block)
-    trial_carrier   = Instance(blocks.Block)
-    trial_modulator = Instance(blocks.Block)
-    output_int      = Instance(Sink)
-    output_trial    = Instance(Sink)
+    def _generate_trial_waveform(self):
+        # Use BBN
+        level = self.get_current_value('level')
+        seed = self.get_current_value('seed')
+        onset = self.get_current_value('modulation_onset')
+        depth = self.get_current_value('modulation_depth')
+        direction = self.get_current_value('modulation_direction')
+        fm = self.get_current_value('fm')
+        duration = self.get_current_value('duration')
 
-    def _output_int_default(self):
-        return Sink(token=self.int_carrier.waveform, fs=self.iface_behavior.fs,
-                calibration=self.cal_primary)
+        t = signal.time(self.iface_behavior.fs, duration)
 
-    def _output_trial_default(self):
-        return Sink(token=self.trial_modulator.waveform,
-                fs=self.iface_behavior.fs, calibration=self.cal_primary)
+        state = np.random.RandomState(seed)
+        waveform = state.uniform(low=-1, high=1, size=len(t))
+        rms = signal.rms(waveform)
+        waveform = amplitude/rms*samples
+
+        eq_phase = signals.am_eq_phase(depth, direction)
+        eq_power = signals.am_eq_power(depth)
+        envelope = depth/2.0 * np.cos(2*np.pi*fm+eq_phase) + 1 - depth/2.0
+        envelope *= 1/eq_power
+        return waveform*envelope
 
     def set_speaker_equalize(self, value):
-        self.output_trial.equalize = value
-        self.output_int.equalize = value
+        if value:
+            raise NotImplementedError, "Cannot equalize signal"
     
-    def set_level(self, value):
-        self.int_carrier.level = value
-        self.trial_carrier.level = value
-
-    def _trial_carrier_default(self):
-        return blocks.BroadbandNoise()
-
-    def _int_carrier_default(self):
-        return blocks.BroadbandNoise()
-
-    def _trial_modulator_default(self):
-        return blocks.SAM(token=self.trial_carrier.waveform, 
-                equalize_power=True,
-                equalize_phase=True)
-        
-    def set_seed(self, value):
-        self.int_carrier.seed = value
-        self.trial_carrier.seed = value
-
-    def set_modulation_onset(self, value):
-        self.trial_modulator.delay = value
-
-    def set_modulation_depth(self, value):
-        self.trial_modulator.depth = value
-
-    def set_modulation_direction(self, value):
-        self.trial_modulator.equalize_direction = value
-
-    def set_trial_duration(self, value):
-        super(AversiveAMNoiseControllerMixin, self).set_trial_duration(value)
-        self.output_int.duration = value
-        self.output_trial.duration = value
-
-    def set_fm(self, value):
-        self.trial_modulator.frequency = value
-
     def update_intertrial(self):
         samples = self.buffer_int.available()
-        att, waveform, clip, floor = self.output_int.realize_samples(samples)
+        state = np.random.RandomState() # Seed will be "random"
+        waveform = state.uniform(low=-1, high=1, size=samples)
+        rms = signal.rms(waveform)
+        waveform = amplitude/rms*waveform
         self.buffer_int.write(waveform)
 
     def update_trial(self):
         speaker = self.get_current_value('speaker')
         if speaker == 'primary':
-            self.output_trial.calibration = self.cal_primary
-            att1, waveform, clip, floor = self.output_trial.realize()
+            waveform, att1 = self._generate_trial_waveform(self.cal_primary)
             att2 = 120.0
         elif speaker == 'secondary':
-            self.output_trial.calibration = self.cal_secondary
-            att2, waveform, clip, floor = self.output_trial.realize()
+            waveform, att2 = self._generate_trial_waveform(self.cal_secondary)
             att1 = 120.0
         else:
-            raise ValueError, 'Unsupported speaker mode %r' % speaker
-
+            raise ValueError, 'Unsupported speaker {}'.format(speaker)
         self.set_attenuations(att1, att2, mode='full')
         self.buffer_trial.set(waveform)
 
@@ -119,6 +91,7 @@ class Paradigm(
         AMBBNParadigmMixin,
         ):
 
+    # Override settings 
     repeat_fa = False
     go_probability = 'h_uniform(c_safe, 3, 7)'
     modulation_onset = 0.0
@@ -126,7 +99,6 @@ class Paradigm(
 
     kw = {'context': True, 'store': 'attribute', 'log': True}
 
-    modulation_onset = Expression('uniform(0.2, 0.4)', label='Modulation onset (s)')
     fm = Expression(5, label='Modulation frequency (Hz)', **kw)
     level = Expression(60.0, label='Spectrum Level (dB SPL)', **kw)
     seed = Expression(-1, label='Noise seed', **kw)
@@ -138,7 +110,6 @@ class Paradigm(
     signal_group = VGroup(
             'fm',
             'modulation_depth',
-            'modulation_onset',
             'level',
             'modulation_direction',
             'seed',
