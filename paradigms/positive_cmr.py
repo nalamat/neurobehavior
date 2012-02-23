c58b48175b6875c01fa0669082a0eaad3db5d8fc
Brad Buran <Brad Buran@localhost>
2012-02-23 16:48:50 -0500

    Additional revisions to draft of Antje's CMR paradigm
    * The values of the target and masker (which are defined by the nogo and go
      input files) are now saved to the trial log.
    * These parameters can now be used in the on-line analysis and plotting of the
      psychometric function.

--- paradigms/positive_cmr.py
+++ paradigms/positive_cmr.py
@@ -1,5 +1,7 @@
 from __future__ import division
 
+from os import path
+
 from enthought.traits.api import Instance, File, List, Any, Int, Bool, Enum
 from enthought.traits.ui.api import View, Include, VSplit, VGroup, Item
 from enthought.enable.api import Component, ComponentEditor
@@ -16,8 +18,8 @@ from tdt.device import RZ6
 
 import numpy as np
 
-# __name__ is a special variable available to the module that will be 
-# "launchers.positive_cmr" (i.e. the "name" of the module as seen by Python).
+# __name__ is a special variable available to the module that will be
+# "paradigms.positive_cmr" (i.e. the "name" of the module as seen by Python).
 import logging
 log = logging.getLogger(__name__)
 
@@ -52,7 +54,6 @@ class Controller(
     go_parameters = List   # Technically a list of lists
     nogo_parameters = List # Technically a list of lists
     
-    # If you call 
     random_generator = Any
     random_seed = Int
      
@@ -61,11 +62,10 @@ class Controller(
         # AbstractExperimentController.start() is called.  This method then
         # calls start_experiment() which is defined here.  Note that at the very
         # end of this method, we call
-        # AbstraactPositiveExperiment.start_experiment() to finish the sequence
+        # AbstractPositiveExperiment.start_experiment() to finish the sequence
         # of operations that are required to run the experiment.
 
-        # Generate a random seed from the time in seconds since start of the
-        # Unix epoch
+        # Generate a random seed based on the computer's clock.
         self.random_seed = int(time())
         
         # If we were to call any of the numpy.random functions (e.g.
@@ -95,7 +95,11 @@ class Controller(
         super(Controller, self).start_experiment(info)
    
     def set_hw_att(self, atten):
-        # The RPvds circuit has a toggle to send the waveform to one speaker or the other (the inactive speaker essentially recieves a string of 0's), so we will simply set both speakers to the same attenuation to avoid the "click" in between every trial that occurs when the hardware attenuators are set.
+        # The RPvds circuit has a toggle to send the waveform to one speaker or
+        # the other (the inactive speaker essentially recieves a string of 0's),
+        # so we will simply set both speakers to the same attenuation to avoid
+        # the "click" in between every trial that occurs when the hardware
+        # attenuators are set.
         att_bits = RZ6.atten_to_bits(atten, atten)
         self.iface_behavior.set_tag('att_bits', att_bits)
 
@@ -104,7 +108,14 @@ class Controller(
     
     def set_nogo_filename(self, filename):
         log.debug("Loading nogo settings from {}".format(filename))
-        # Use loadtxt because it convers the data to numeric values rather than loading as a string, then reverse the array and convert it to a list.  We convert to a list because this allows us to take advantage of the pop() method.  Numpy arrays do not have this feature, so we'd have to rewrite other parts of the code if we kept the data structure as an array rather than converting it to a list.In Numpy, extra dimensions are implied in contrast with matlab where you specifically have to provide the : to include them.
+
+        # Use np.loadtxt instead of the csv module because it convers the data
+        # to numeric values rather than loading as a string, then reverse the
+        # array and convert it to a list.  We convert to a list because this
+        # allows us to take advantage of the pop() method.  Numpy arrays do not
+        # have this feature, so we'd have to rewrite other parts of the code if
+        # we kept the data structure as an array rather than converting it to a
+        # list.
         self.nogo_parameters = np.loadtxt(filename, delimiter=',')[::-1].tolist()
 
     def set_go_filename(self, filename):
@@ -120,9 +131,18 @@ class Controller(
         self.go_parameters = go_parameters
 
     def trigger_next(self):
-        # This function is *required* to be called.  This basically calls the logic (defined in AbstractExperimentController) which clears the current value of all parameters and recomputes them (this is important in between trials).
+        # This function is *required* to be called.  This basically calls the
+        # logic (defined in AbstractExperimentController) which clears the
+        # current value of all parameters and recomputes them (this is important
+        # in between trials).
         self.invalidate_context()
-        # This must be called before every trial to load (or evaluate if the parameter is an expression) the values of each parameter.  Note this method will also check to see if the value of a parameter has changed since the last trial.  If so, the corresponding set_parametername method will be called with the new value as an argument.
+
+        # This must be called before the start of every trial to load (or
+        # evaluate if the parameter is an expression) the values of each
+        # parameter.  Note this method will also check to see if the value of a
+        # parameter has changed since the last trial.  If so, the corresponding
+        # set_parametername method will be called with the new value as an
+        # argument.
         self.evaluate_pending_expressions()
 
         # For all variables declared as context=True, you can get the current
@@ -157,9 +177,9 @@ class Controller(
             # This is the very first trial
             ttype = 'GO_REMIND'
             settings = self.go_remind
-        # When the user clicks on the "remind" button, it sets the
-        # remind_requested attribute on the experiment controller to True.
         elif self.remind_requested:
+            # When the user clicks on the "remind" button, it sets the
+            # remind_requested attribute on the experiment controller to True.
             ttype = 'GO_REMIND'
             settings = self.go_remind
         elif nogo and spout and repeat_fa:
@@ -178,7 +198,7 @@ class Controller(
         # returned
         
         # "unpack" our list into individual variables
-        F, E, FC, TL, ML, TokenNo, TargetNo = settings
+        F, E, FC, ML, TL, TokenNo, TargetNo = settings
         
         masker_file = r'E:\programs\ANTJE CMR\CMR\stimuli\M{}{}{}{}.stim'.format(int(F), int(E), int(FC), int(TokenNo))
         target_file = r'E:\programs\ANTJE CMR\CMR\stimuli\T{}{}.stim'.format(int(FC), int(TargetNo))
@@ -190,14 +210,17 @@ class Controller(
         # you are playing a tone at the specified frequency and voltage (i.e.
         # Vrms)
         dBSPL_RMS1 = cal.get_spl(frequencies=1e3, voltage=1)
-        # Scale waveforms so that we get desired stimulus level assuming 0 dB of attenuation
-        
+
+        # Scale waveforms so that we get desired stimulus level assuming 0 dB of
+        # attenuation
         masker = 10**((ML-dBSPL_RMS1)/20)*masker
         target = 10**((TL-dBSPL_RMS1)/20)*target
         stimulus = target + masker     
         
         stimulus = stimulus * 10**(hw_att/20)
-        # Set the flag in the RPvds circuit that indicates which output to send the waveform to
+
+        # Set the flag in the RPvds circuit that indicates which output to send
+        # the waveform to
         if speaker == 'primary':
             self.iface_behavior.set_tag('speaker', 0)
         elif speaker == 'secondary':
@@ -205,18 +228,43 @@ class Controller(
 
         self.buffer_out.set(stimulus)
     
-        # Be sure that the trial type is added to the list of context variables that will be saved to the trial_log file.
+        # Be sure that the trial type is added to the list of context variables
+        # that will be saved to the trial_log file.
         self.set_current_value('ttype', ttype)
 
-        # Boolean flag in the circuit that indicates whether or not the current trial is a go.  This ensures that a reward is not delivered on NOGO trials.
+        # Boolean flag in the circuit that indicates whether or not the current
+        # trial is a go.  This ensures that a reward is not delivered on NOGO
+        # trials.
         if ttype.startswith('GO'):
             self.iface_behavior.set_tag('go?', 1)
         else:
             self.iface_behavior.set_tag('go?', 0)
         
-        # This is a "handshake" that lets the RPvds circuit know that we are done with preparations for the next trial (e.g. uploading the stimulus waveform).  The RPvds circuit will not proceed with the next trial until it receives a "SoftTrig1" (in TDT parlance)
+        self.set_current_value('target_level', TL)
+        self.set_current_value('masker_level',ML)
+        self.set_current_value('TMR',TL-ML)
+        self.set_current_value('target_number',TargetNo)
+        self.set_current_value('masker_number',TokenNo)
+        self.set_current_value('masker_envelope',E)
+        self.set_current_value('masker_flanker',F)
+        self.set_current_value('center_frequency',FC)
+
+        # This is a "handshake" that lets the RPvds circuit know that we are
+        # done with preparations for the next trial (e.g. uploading the stimulus
+        # waveform).  The RPvds circuit will not proceed with the next trial
+        # until it receives a "SoftTrig1" (in TDT parlance)
         self.iface_behavior.trigger(1)
 
+    def log_trial(self, **kwargs):
+        # HDF5 data files do not natively support unicode strings so we need to
+        # convert our filename to an ASCII string.  While we're at it, we should
+        # probably strip the directory path as well and just save the basename.
+        go_filename = self.get_current_value('go_filename')
+        kwargs['go_filename'] = str(path.basename(go_filename))
+        nogo_filename = self.get_current_value('nogo_filename')
+        kwargs['nogo_filename'] = str(path.basename(nogo_filename))
+        super(Controller, self).log_trial(**kwargs)
+    
 class Paradigm(
         AbstractPositiveParadigm, 
         PumpParadigmMixin,
@@ -229,8 +277,17 @@ class Paradigm(
     go_probability = Expression('0.5 if c_nogo < 5 else 1', 
             label='Go probability', log=False, context=True)
     repeat_fa = Bool(True, label='Repeat if FA?', log=True, context=True)
-    nogo_filename = File(context=True, log=True, label='NOGO filename')
-    go_filename = File(context=True, log=True, label='GO filename')
+    nogo_filename = File(context=True, log=False, label='NOGO filename')
+    go_filename = File(context=True, log=False, label='GO filename')
+
+    target_level = Int(label='Target Level', log=True, context=True)
+    masker_level = Int(label='Masker Level', log=True, context=True)
+    TMR = Int(label='Target Level', log=True, context=True)
+    target_number = Int(label='Target Token Number', log=True, context=True)
+    masker_number = Int(label='Masker Token Number', log=True, context=True)
+    masker_envelope = Int(label='Masker Envelope', log=True, context=True)
+    masker_flanker = Int(label='Masker with or without Flanker', log=True, context=True)
+    center_frequency = Int(label='Masker with or without Flanker', log=True, context=True)
     
     hw_att = Enum(0, 20, 40, 60, context=True, log=True, label='HW attenuation (dB)')
     
