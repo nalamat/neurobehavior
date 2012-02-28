60c96446efb3d6a359edf3a0be92a906d52cbca3
brad <brad@phoenix.nyu.edu>
2012-02-28 14:53:34 -0500

    Getting rid of the sane module and moving code back into Neurobehavior
    I had originally envisioned two separate packages.  One for online data
    collection and plotting (Neurobehavior) and one for offline data analysis
    (sane); however, it has become clear to me that some of the code between these
    two packages still would need to be shared (e.g. the off-line plotting of the
    physiology data still depends on components that live in Neurobehavior).  Rather
    than having two packages, I'm going to merge them back together.

---
+++ cns/io.py
@@ -0,0 +1,44 @@
+import tables
+
+def get_experiment_node(filename=None):
+    '''
+    Given a physiology experiment file with multiple experiments, prompt the
+    user for the experiment they'd like to analyze.  If only one experiment is
+    present, no prompt will be generated.
+
+    filename : str or None
+        File to obtain experiment from.  If None, extract the argument from
+        sys.argv[1]
+
+    returns : (filename, node path)
+    '''
+    if filename is None:
+        import sys
+        filename = sys.argv[1]
+
+    with tables.openFile(filename, 'r') as fh:
+        nodes = fh.root._f_listNodes()
+        if len(nodes) == 1:
+            return filename, nodes[0]._v_pathname
+        elif len(nodes) == 0:
+            return ''
+
+        while True:
+            print 'Available experiments to analyze'
+            for i, node in enumerate(nodes):
+                try:
+                    trials = len(node.data.trial_log)
+                except:
+                    trials = 0
+                print '{}. {} trials: {}'.format(i, trials, node._v_name)
+
+            ans = raw_input('Which experiment would you like to analyze? ')
+            try:
+                ans = int(ans)
+                if 0 <= ans < len(nodes):
+                    break
+                else:
+                    print 'Invalid option'
+            except ValueError:
+                print 'Please enter the number of the experiment'
+        return filename, nodes[ans]._v_pathname
