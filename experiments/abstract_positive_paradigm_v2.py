aeaf4a06090a2f933c9e6e50d125d9f216af255d
Brad Buran <Brad Buran@localhost>
2012-03-20 20:32:38 -0400

    Renamed GvT's revisions to the appetitive paradigm as v3
    This will allow us to keep v2 of the paradigm (which defines a reaction window)
    around for posterity.  GvT's version does not define a reaction window, but
    allows for a response window delay.  All future paradigms should use GvT's.

---
+++ experiments/abstract_positive_paradigm_v2.py
@@ -0,0 +1,47 @@
+from enthought.traits.api import Float
+from enthought.traits.ui.api import View, VGroup, Include
+
+from .abstract_experiment_paradigm import AbstractExperimentParadigm
+from .evaluate import Expression
+
+class AbstractPositiveParadigm(AbstractExperimentParadigm):
+    
+    kw = {'context': True, 'store': 'attribute', 'log': True}
+
+    auto_iti = Expression('uniform(1, 2)', label='ITI for automatic mode', **kw)
+    pump_rate = Expression(1.5, label='Pump rate (ml/min)', **kw)
+    reward_volume = Expression(25, label='Reward volume (ul)', **kw)
+    signal_offset_delay = Expression(0.5, label='Signal offset delay (s)', **kw)
+    intertrial_duration = Expression(0.1, label='Intertrial duration (s)', **kw)
+    reaction_window_delay = Expression(0, label='Withdraw delay (s)', **kw)
+    reaction_window_duration = Expression(1.5, label='Withdraw duration (s)', **kw)
+    response_window_duration = Expression(3, label='Response duration (s)', **kw)
+    timeout_duration = Expression(1, label='TO duration (s)', **kw)
+    poke_duration = Expression(0.2, label='Poke duration (s)', **kw)
+
+    mic_fhp = Float(100, label='Microphone highpass cutoff (Hz)', **kw)
+    mic_flp = Float(40e3, label='Microphone lowpass cutoff (Hz)', **kw)
+
+    abstract_positive_paradigm_group = VGroup(
+            'auto_iti',
+            'mic_fhp',
+            'mic_flp',
+            'signal_offset_delay',
+            'intertrial_duration',
+            'reaction_window_delay',
+            'reaction_window_duration',
+            'response_window_duration',
+            'timeout_duration',
+            'poke_duration',
+            'pump_rate',
+            'reward_volume',
+            label='Paradigm',
+            show_border=True,
+            )
+
+    traits_view = View(
+            Include('abstract_positive_paradigm_group'),
+            resizable=True,
+            title='Abstract positive paradigm editor',
+            width=100,
+            )
