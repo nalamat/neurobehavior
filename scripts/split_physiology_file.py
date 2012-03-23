c3a03e160f15fc96992a93a513d90bb3c19b36bd
brad <brad@phoenix.nyu.edu>
2012-03-22 23:27:23 -0400

    Revamped split_physiology_file script to take list of filenames
    The original behavior was to take a directory and glob it for *.hd5 files.  Now,
    you can pass a list of specific files to be processed via the command-line.

--- scripts/split_physiology_file.py
+++ scripts/split_physiology_file.py
@@ -1,16 +1,21 @@
+import re
+import tables 
+
+def main(filename):
+    with tables.openFile(filename, 'r') as fh:
+        if fh.root._v_nchildren != 1:
+            filename_pattern = re.sub(r'(.*)\.hd5', r'\1_v{}.hd5', filename)
+            for i, node in enumerate(fh.root._f_listNodes()):
+                new_filename = filename_pattern.format(i)
+                with tables.openFile(new_filename, 'w') as fh_out:
+                    print 'Moving {} to {}'.format(node._v_pathname,
+                                                   new_filename)
+                    node._f_copy(fh_out.root, recursive=True)
+        else:
+            print 'Only one experiment node in {}.  Skipping.'.format(filename)
+
 if __name__ == '__main__':
-    import re
-    import tables 
     import sys
-    from os import path
-    from glob import glob
-    pathname = sys.argv[1]
-    globpattern = path.join(pathname, '*.hd5')
-    for filename in glob(globpattern):
-        with tables.openFile(filename, 'r') as fh:
-            if fh.root._v_nchildren != 1:
-                filename_pattern = re.sub(r'(.*)\.hd5', r'\1_v{}.hd5', filename)
-                for i, node in enumerate(fh.root._f_listNodes()):
-                    new_filename = filename_pattern.format(i)
-                    with tables.openFile(new_filename, 'w') as fh_out:
-                        node._f_copy(fh_out.root, recursive=True)
+    for filename in sys.argv[1:]:
+        print 'Splitting {}'.format(filename)
+        main(filename)
