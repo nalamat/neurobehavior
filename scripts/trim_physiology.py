669608883d54d7109ea47b9de00d0043023f55e4
brad <brad@phoenix>
2012-05-31 13:16:44 -0400

    Added docstring

--- scripts/trim_physiology.py
+++ scripts/trim_physiology.py
@@ -3,6 +3,11 @@ from cns import h5
 from cns import analysis
 
 def main(filename):
+    '''
+    Removes artifacts typically found at the beginning and end of an experiment
+    (e.g. when putting the animal in the cage or when the headstage falls off at
+    the end of the experiment).
+    '''
     with tables.openFile(filename, 'a') as fh:
         exp_node = h5.p_get_node(fh, '*')
         start = exp_node.data.trial_log.cols.start[0] - 5
