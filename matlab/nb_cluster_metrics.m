8b0939b0a49d0fd0df30a506da3c97c02e597f08
brad <brad@phoenix.nyu.edu>
2012-06-15 10:27:51 -0400

    Enhanced support for saving cluster metrics to the curated spiketimes file
    * Script nb_annotate_curated will analyze the curated clusters in the file and
      add some quality  metrics.
    * nb_cluster_metrics updated to report all statistics as fractions rather than
      percents to avoid any confusion or ambiguity.

--- matlab/nb_cluster_metrics.m
+++ matlab/nb_cluster_metrics.m
@@ -9,41 +9,45 @@ function [stats, fp, fn] = nb_cluster_metrics(spikes)
 %   Returns
 %   -------
 %   stats
-%       M x 6 array where each row represents the cluster and each column i sa
+%       M x 8 array where each row represents the cluster and each column is a
 %       different statistic.  M is number of clusters.  Columns represent the
-%       following statistics (in order)
-%           lower bound of 95% CI for %RPV contaminatoin
-%           upper bound of 95% CI for %RPV contaminatoin
-%           expected %RPV contamination
+%       following statistics (in order):
+%           lower bound of 95% CI for RPV contamination (fraction)
+%           upper bound of 95% CI for RPV contamination (fraction)
+%           expected RPV contamination (fraction)
 %           actual number of RPVs
 %           total number of spikes
-%           probability of missing spikes based on threshold
+%           actual RPV contamination (fraction)
+%           probability of missing spikes based on threshold (fraction)
+%           ISI quality metric
 %   fp
 %       Probability of false positives
 %   fn
 %       Probability of false negatives
 
     n_clusters = size(spikes.labels, 1);
-    stats = zeros(size(spikes.labels, 1), 7);
+    stats = zeros(size(spikes.labels, 1), 8);
 
     for cluster = 1:n_clusters
         cluster_id = spikes.labels(cluster,1);
-        % Returns bounds of 95% CI, expected value of % contamination and actual
-        [lb, ub, p, c] = ss_rpv_contamination(spikes, cluster_id);
+        % Returns bounds of 95% CI, expected value of % contamination and
+        % actual.  Convert this to a fraction rather than a percent.
+        [lb, ub, erpv, nrpv] = ss_rpv_contamination(spikes, cluster_id);
         indices = get_spike_indices(spikes, cluster_id);
-        stats(cluster, 1:5) = [lb, ub, p, c, size(indices, 2)];
+        ntot = size(indices, 2);
+        rpv = nrpv/ntot;
+        stats(cluster, 1:6) = [lb/100, ub/100, erpv/100, nrpv/100, ntot, rpv];
 
         % Returns probability of missing spike, estimate of mode for minimum
         % values, standard deviation for distribution of minimum values.
-        stats(cluster, 6) = ss_undetected(spikes, cluster_id);
+        stats(cluster, 7) = ss_undetected(spikes, cluster_id);
 
-        % foo
         ts = spikes.unwrapped_times(indices);
         isi = diff(sort(ts));
         f2 = numel(find((isi >= 1.2e-3) & (isi < 2e-3)));
         f10 = numel(find((isi >= 2e-3) & (isi < 10e-3)));
         r = (8.8/0.8) * (f2/f10);
-        stats(cluster, 7) = r;
+        stats(cluster, 8) = r;
     end
 
     fp = zeros(n_clusters);
@@ -53,6 +57,10 @@ function [stats, fp, fn] = nb_cluster_metrics(spikes)
         for j = (i+1):n_clusters
             j_id = spikes.labels(j, 1);
             C = ss_gaussian_overlap(spikes, i_id, j_id);
+            % The rows in the fp and fn matrices reflect the contribution of
+            % false positives/negatives from other clusters to the given
+            % cluster.  sum(fp(i,:)) will give you total false positives for the
+            % specified cluster.
             fp(i,j) = C(1,1); % false positive in i (assigned from j)
             fp(j,i) = C(2,2); % false positive in j (assigned from i)
             fn(i,j) = C(1,2); % false negative in i (assigned to j)
