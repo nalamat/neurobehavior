c96b5fe0a358f67f7a2045fc6fccd00f4801ec3d
Nima Alamatsaz <nima.alamatsaz@gmail.com>
2016-06-04 20:17:26 -0400

    Add modulated masker and target phase delay

--- experiments/abstract_positive_paradigm_v3.py
+++ experiments/abstract_positive_paradigm_v3.py
@@ -8,14 +8,15 @@ class AbstractPositiveParadigm(AbstractExperimentParadigm):
 
     kw = {'context': True, 'log': True}
 
-    #pump_rate = Expression(1.5, label='Pump rate (ml/min)', **kw)
-    #reward_volume = Expression(25, label='Reward volume (ul)', **kw)
-
-    iti_duration = Expression(0.1, label='Intertrial duration (s)', **kw)
-    to_duration = Expression(1, label='TO duration (s)', **kw)
-    hold_duration = Expression(0, label='Hold duration (s)', **kw)
-    response_duration = Expression(3, label='Response duration (s)', **kw)
-    np_duration = Expression(0.2, label='Poke duration (s)', **kw)
+    iti_duration      = Expression(0.1, label='Intertrial duration (s)', **kw)
+    to_duration       = Expression(1  , label='TO duration (s)'        , **kw)
+    hold_duration     = Expression(0  , label='Hold duration (s)'      , **kw)
+    response_duration = Expression(3  , label='Response duration (s)'  , **kw)
+    np_duration       = Expression(0.2, label='Poke duration (s)'      , **kw)
+    masker_frequency  = Expression(10 , label='Masker frequency (Hz)'  , **kw)
+    phase_delay       = Expression(45 , label='Phase delay (deg)'      , **kw)
+    pump_rate         = Expression(1.5, label='Pump rate (ml/min)'     , **kw)
+    reward_volume     = Expression(25 , label='Reward volume (ul)'     , **kw)
 
     abstract_positive_paradigm_group = VGroup(
             'iti_duration',
@@ -23,8 +24,10 @@ class AbstractPositiveParadigm(AbstractExperimentParadigm):
             'response_duration',
             'to_duration',
             'np_duration',
-            #'pump_rate',
-            #'reward_volume',
+            'masker_frequency',
+            'phase_delay',
+            'pump_rate',
+            'reward_volume',
             label='Paradigm',
             show_border=True,
             )
