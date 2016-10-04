87272541ea6c6851142d711a6162e7ecea71a57d
Nima Alamatsaz <nima.alamatsaz@gmail.com>
2016-10-04 10:31:48 -0400

    Update PhysiologyController to support NI DAQ

--- experiments/physiology_controller.py
+++ experiments/physiology_controller.py
@@ -1,12 +1,18 @@
 from traits.api import (Instance, Any, List, on_trait_change, Enum,
-                                  Dict)
+                                  Dict, Float)
 
 from traitsui.api import Controller
 from pyface.timer.api import Timer
 
 from cns import get_config
+from cns.channel import FileChannel, FileMultiChannel, MultiChannel
 from os.path import join
 from cns.pipeline import deinterleave_bits
+
+import numpy as np
+import threading
+
+from daqengine.ni import Engine
 
 CHANNELS = get_config('PHYSIOLOGY_CHANNELS')
 PHYSIOLOGY_WILDCARD = get_config('PHYSIOLOGY_WILDCARD')
@@ -17,17 +23,19 @@ from .utils import load_instance, dump_instance
 class PhysiologyController(Controller):
 
     buffer_                 = Any
-    iface_physiology        = Any
-    buffer_raw              = Any
-    buffer_proc             = Any
-    buffer_ts               = Any
+    #iface_physiology        = Any
+    buffer_raw              = Instance(FileMultiChannel)
+    buffer_proc             = Instance(FileMultiChannel)
+    buffer_ts               = Instance(FileMultiChannel)
     buffer_ttl              = Any
     physiology_ttl_pipeline = Any
     buffer_spikes           = List(Any)
     state                   = Enum('master', 'client')
-    process                 = Instance('tdt.DSPProject')
+    #process                 = Instance('tdt.DSPProject')
     timer                   = Instance(Timer)
     parent                  = Any
+    fs                      = Float
+    _lock = threading.Lock()
 
     shell_variables         = Dict
 
@@ -39,12 +47,18 @@ class PhysiologyController(Controller):
     def _shell_variables_default(self):
         return dict(controller=self, c=self)
 
+    def _buffer_raw_default(self):
+        # return FileChannel(node=self.store_node, name='raw', dtype=np.float32)
+        return FileMultiChannel(node=self.parent.model.data.store_node, channels=16,
+                                name='raw', dtype=np.float32,
+                                compression_type='lzo', compression_level=1,
+                                use_shuffle=True, use_checksum=True)
     def init(self, info):
-        self.setup_physiology()
         self.model = info.object
+        self.setup_physiology()
         if self.state == 'master':
-            self.process.start()
-            self.iface_physiology.trigger('A', 'high')
+            #self.process.start()
+            #self.iface_physiology.trigger('A', 'high')
             self.start()
 
     def close(self, info, is_ok, from_parent=False):
@@ -67,31 +81,51 @@ class PhysiologyController(Controller):
                 return False
 
     def setup_physiology(self):
-        # Load the circuit
-        circuit = join(get_config('RCX_ROOT'), 'physiology')
-        self.iface_physiology = self.process.load_circuit(circuit, 'RZ5')
+        self.fs = 500e3/16
+        self.model.data.raw.fs = self.fs
+        self.model.data.processed.fs = self.fs
+        # # Load the circuit
+        #
+        # circuit = join(get_config('RCX_ROOT'), 'physiology')
+        # self.iface_physiology = self.process.load_circuit(circuit, 'RZ5')
+        #
+        # # Initialize the buffers that will be spooling the data
+        # self.buffer_raw = self.iface_physiology.get_buffer('craw',
+        #         'r', src_type='float32', dest_type='float32', channels=CHANNELS,
+        #         block_size=1048)
+        # self.buffer_filt = self.iface_physiology.get_buffer('cfilt',
+        #         'r', src_type='int16', dest_type='float32', channels=CHANNELS,
+        #         block_size=1048)
+        # self.buffer_ts = self.iface_physiology.get_buffer('trig/',
+        #         'r', src_type='int32', dest_type='int32', block_size=1)
+        # self.buffer_ts_start = self.iface_physiology.get_buffer('trig/',
+        #         'r', src_type='int32', dest_type='int32', block_size=1)
+        # self.buffer_ts_end = self.iface_physiology.get_buffer('trig\\',
+        #         'r', src_type='int32', dest_type='int32', block_size=1)
+        # self.buffer_ttl = self.iface_physiology.get_buffer('TTL',
+        #         'r', src_type='int8', dest_type='int8', block_size=1)
+        #
+        # for i in range(CHANNELS):
+        #     name = 'spike{}'.format(i+1)
+        #     buffer = self.iface_physiology.get_buffer(name, 'r',
+        #             block_size=SPIKE_SNIPPET_SIZE+2)
+        #     self.buffer_spikes.append(buffer)
+        pass
 
-        # Initialize the buffers that will be spooling the data
-        self.buffer_raw = self.iface_physiology.get_buffer('craw',
-                'r', src_type='float32', dest_type='float32', channels=CHANNELS,
-                block_size=1048)
-        self.buffer_filt = self.iface_physiology.get_buffer('cfilt',
-                'r', src_type='int16', dest_type='float32', channels=CHANNELS,
-                block_size=1048)
-        self.buffer_ts = self.iface_physiology.get_buffer('trig/',
-                'r', src_type='int32', dest_type='int32', block_size=1)
-        self.buffer_ts_start = self.iface_physiology.get_buffer('trig/',
-                'r', src_type='int32', dest_type='int32', block_size=1)
-        self.buffer_ts_end = self.iface_physiology.get_buffer('trig\\',
-                'r', src_type='int32', dest_type='int32', block_size=1)
-        self.buffer_ttl = self.iface_physiology.get_buffer('TTL',
-                'r', src_type='int8', dest_type='int8', block_size=1)
+    def start_physiology(self, info=None):
+        # print '#############################################'
+        # print 'Starting physiology'
+        # print '#############################################'
 
-        for i in range(CHANNELS):
-            name = 'spike{}'.format(i+1)
-            buffer = self.iface_physiology.get_buffer(name, 'r',
-                    block_size=SPIKE_SNIPPET_SIZE+2)
-            self.buffer_spikes.append(buffer)
+        channels = []
+        for i in range(0, self.parent.model.data.channels): channels.append('ch' + str(i))
+        self.parent.engine.configure_hw_ai2(self.fs, '/Dev1/ai0:15', (-10, 10), names=channels)
+        self.parent.engine.register_ai2_callback(self.samples_acquired)
+
+    def samples_acquired(self, names, samples):
+        self.model.data.raw.send(samples)
+        self.model.data.processed.send(samples)
+        # self.buffer_raw.send(samples)
 
     @on_trait_change('model.data')
     def update_data(self):
@@ -112,11 +146,12 @@ class PhysiologyController(Controller):
         self.physiology_ttl_pipeline = deinterleave_bits(targets)
 
     def start(self):
-        self.timer = Timer(100, self.monitor_physiology)
+        # self.timer = Timer(100, self.monitor_physiology)
+        pass
 
     def stop(self):
         self.timer.stop()
-        self.process.stop()
+        # self.process.stop()
 
     def monitor_physiology(self):
         # Acquire raw physiology data
@@ -124,85 +159,94 @@ class PhysiologyController(Controller):
         self.model.data.raw.send(waveform)
 
         # Acquire filtered physiology data
-        waveform = self.buffer_filt.read()
+        # waveform = self.buffer_filt.read()
         self.model.data.processed.send(waveform)
 
         # Acquire sweep data
-        ttl = self.buffer_ttl.read()
-        self.physiology_ttl_pipeline.send(ttl)
+        # ttl = self.buffer_ttl.read()
+        # self.physiology_ttl_pipeline.send(ttl)
 
         # Get the timestamps
-        ts = self.buffer_ts.read().ravel()
-        self.model.data.ts.send(ts)
+        # ts = self.buffer_ts.read().ravel()
+        # self.model.data.ts.send(ts)
 
-        ends = self.buffer_ts_end.read().ravel()
-        starts = self.buffer_ts_start.read(len(ends)).ravel()
-        self.model.data.epoch.send(zip(starts, ends))
+        # ends = self.buffer_ts_end.read().ravel()
+        # starts = self.buffer_ts_start.read(len(ends)).ravel()
+        # self.model.data.epoch.send(zip(starts, ends))
 
         # Get the spikes.  Each channel has a separate buffer for the spikes
         # detected online.  Need to add 2 to the snippet size to compensate for
         # the extra samples provided with the snippet (the timestamp and the
         # classifier).
-        snippet_shape = (-1, SPIKE_SNIPPET_SIZE+2)
-        for i in range(CHANNELS):
-            data = self.buffer_spikes[i].read().reshape(snippet_shape)
-
-            # First sample of each snippet is the timestamp (as a 32 bit
-            # integer) and last sample is the classifier (also as a 32 bit
-            # integer).  The bits in between should be interpreted as 32-bit
-            # floating point values.
-            snip = data[:,1:-1]
-            ts = data[:,0].view('int32')
-            cl = data[:,-1].view('int32')
-            self.model.data.spikes[i].send(snip, ts, cl)
+        # snippet_shape = (-1, SPIKE_SNIPPET_SIZE+2)
+        # for i in range(CHANNELS):
+        #     data = self.buffer_spikes[i].read().reshape(snippet_shape)
+        #
+        #     # First sample of each snippet is the timestamp (as a 32 bit
+        #     # integer) and last sample is the classifier (also as a 32 bit
+        #     # integer).  The bits in between should be interpreted as 32-bit
+        #     # floating point values.
+        #     snip = data[:,1:-1]
+        #     ts = data[:,0].view('int32')
+        #     cl = data[:,-1].view('int32')
+        #     self.model.data.spikes[i].send(snip, ts, cl)
 
     @on_trait_change('model.settings.spike_signs')
     def set_spike_signs(self, value):
         for ch, sign in enumerate(value):
             name = 's_spike{}'.format(ch+1)
-            self.iface_physiology.set_tag(name, sign)
+            # self.iface_physiology.set_tag(name, sign)
 
     @on_trait_change('model.settings.spike_thresholds')
     def set_spike_thresholds(self, value):
         for ch, threshold in enumerate(value):
             name = 'a_spike{}'.format(ch+1)
-            self.iface_physiology.set_tag(name, threshold)
+            # self.iface_physiology.set_tag(name, threshold)
 
     @on_trait_change('model.settings.monitor_fc_highpass')
     def set_monitor_fc_highpass(self, value):
-        self.iface_physiology.set_tag('FiltHP', value)
+        # self.iface_physiology.set_tag('FiltHP', value)
+        pass
 
     @on_trait_change('model.settings.monitor_fc_lowpass')
     def set_monitor_fc_lowpass(self, value):
-        self.iface_physiology.set_tag('FiltLP', value)
+        # self.iface_physiology.set_tag('FiltLP', value)
+        pass
 
     @on_trait_change('model.settings.monitor_ch_1')
     def set_monitor_ch_1(self, value):
-        self.iface_physiology.set_tag('ch1_out', value)
+        # self.iface_physiology.set_tag('ch1_out', value)
+        pass
 
     @on_trait_change('model.settings.monitor_ch_2')
     def set_monitor_ch_2(self, value):
-        self.iface_physiology.set_tag('ch2_out', value)
+        # self.iface_physiology.set_tag('ch2_out', value)
+        pass
 
     @on_trait_change('model.settings.monitor_ch_3')
     def set_monitor_ch_3(self, value):
-        self.iface_physiology.set_tag('ch3_out', value)
+        # self.iface_physiology.set_tag('ch3_out', value)
+        pass
 
     @on_trait_change('model.settings.monitor_gain_1')
     def set_monitor_gain_1(self, value):
-        self.iface_physiology.set_tag('ch1_out_sf', value*1e3)
+        # self.iface_physiology.set_tag('ch1_out_sf', value*1e3)
+        pass
 
     @on_trait_change('model.settings.monitor_gain_2')
     def set_monitor_gain_2(self, value):
-        self.iface_physiology.set_tag('ch2_out_sf', value*1e3)
+        # self.iface_physiology.set_tag('ch2_out_sf', value*1e3)
+        pass
 
     @on_trait_change('model.settings.monitor_gain_3')
     def set_monitor_gain_3(self, value):
-        self.iface_physiology.set_tag('ch3_out_sf', value*1e3)
+        # self.iface_physiology.set_tag('ch3_out_sf', value*1e3)
+        pass
 
     @on_trait_change('model.settings.diff_matrix')
     def set_diff_matrix(self, value):
-        self.iface_physiology.set_coefficients('diff_map', value.ravel())
+        # self.iface_physiology.set_coefficients('diff_map', value.ravel())
+        pass
 
     def load_settings(self, info):
         instance = load_instance(PHYSIOLOGY_ROOT, PHYSIOLOGY_WILDCARD)
