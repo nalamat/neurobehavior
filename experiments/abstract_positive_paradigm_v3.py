b8a582b64f1c2786552436318dbd346926834c8a
Nima Alamatsaz <nima.alamatsaz@gmail.com>
2017-07-19 17:23:44 -0400

    Add poke hold duration

--- experiments/abstract_positive_paradigm_v3.py
+++ experiments/abstract_positive_paradigm_v3.py
@@ -9,15 +9,16 @@ class AbstractPositiveParadigm(AbstractExperimentParadigm):
     kw = {'context': True, 'log': True}
     kw2 = {'context': False, 'log': False}
 
-    iti_duration      = Expression(0.1, label='Intertrial duration (s)', **kw)
-    to_duration       = Expression(1  , label='TO duration (s)'        , **kw)
-    hold_duration     = Expression(0.2, label='Hold duration (s)'      , **kw)
-    response_duration = Expression(3  , label='Response duration (s)'  , **kw)
-    poke_duration     = Expression(0.2, label='Poke duration (s)'      , **kw)
-    phase_delay       = Expression(45 , label='Phase delay (deg)'      , **kw)
-    pump_rate         = Expression(1.5, label='Pump rate (ml/min)'     , **kw)
-    reward_volume     = Expression(25 , label='Reward volume (ul)'     , **kw)
-    target_duration   = Expression(1  , label='Target duration (s)'      , **kw)
+    iti_duration       = Expression(0.1, label='Intertrial duration (s)', **kw)
+    to_duration        = Expression(1  , label='TO duration (s)'        , **kw)
+    hold_duration      = Expression(0.2, label='Hold duration (s)'      , **kw)
+    response_duration  = Expression(3  , label='Response duration (s)'  , **kw)
+    poke_duration      = Expression(0.2, label='Poke duration (s)'      , **kw)
+    poke_hold_duration = Expression(0. , label='Poke hold duration (s)' , **kw)
+    phase_delay        = Expression(45 , label='Phase delay (deg)'      , **kw)
+    pump_rate          = Expression(1.5, label='Pump rate (ml/min)'     , **kw)
+    reward_volume      = Expression(25 , label='Reward volume (ul)'     , **kw)
+    target_duration    = Expression(1  , label='Target duration (s)'    , **kw)
 
     abstract_positive_paradigm_group = VGroup(
             'iti_duration',
@@ -25,6 +26,7 @@ class AbstractPositiveParadigm(AbstractExperimentParadigm):
             'response_duration',
             'to_duration',
             'poke_duration',
+            'poke_hold_duration',
             'phase_delay',
             'pump_rate',
             'reward_volume',
